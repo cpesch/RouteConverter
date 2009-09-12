8eb5d2425d4d97aefe0aa6d3f921a53c94495cd1
Christian Pesch <christian_pesch@gmx.de>
2009-09-12 14:57:59 +0000

    stripped JFreeChart
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@1428 964dd122-5732-0410-82e6-8cc08e73365a

--- thirdparty/strip.bat
+++ thirdparty/strip.bat
@@ -3,3 +3,6 @@ java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt djproject\nat
 java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt eclipse\swt\lib\swt-3.5-win32-win32-x86.jar eclipse\swt\lib\swt-3.5-win32-win32-x86-stripped.jar
 java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt eclipse\swt\lib\swt-3.5-win32-win32-x86_64.jar eclipse\swt\lib\swt-3.5-win32-win32-x86_64-stripped.jar
 java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt org\json\090819\json-090819.jar org\json\090819\json-090819-stripped.jar
+java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt jcommon\jcommon\1.0.16\jcommon-1.0.16.jar jcommon\jcommon\1.0.16\jcommon-1.0.16-stripped.jar 
+java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt jfreechart\jfreechart\1.0.13\jfreechart-1.0.13.jar jfreechart\jfreechart\1.0.13\jfreechart-1.0.13-stripped.jar 
+
