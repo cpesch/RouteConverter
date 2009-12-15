f3cf66b9ab48ff854fbbbe28bb44a3370d8d57cf
Christian Pesch <christian_pesch@gmx.de>
2009-12-15 06:35:11 +0000

    who needs thirdparty anyway now that there is Maven?
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@1581 964dd122-5732-0410-82e6-8cc08e73365a

--- thirdparty/strip.bat
+++
@@ -1,9 +0,0 @@
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt apache\commons-httpclient\lib\commons-httpclient-3.1.jar apache\commons-httpclient\lib\commons-httpclient-3.1-stripped.jar
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt djproject\nativeswing\lib\DJNativeSwing-0.9.8.jar djproject\nativeswing\lib\DJNativeSwing-0.9.8-stripped.jar
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt djproject\nativeswing-swt\lib\DJNativeSwing-SWT-0.9.8.jar djproject\nativeswing-swt\lib\DJNativeSwing-SWT-0.9.8-stripped.jar
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt eclipse\swt\lib\swt-3.5-win32-win32-x86.jar eclipse\swt\lib\swt-3.5-win32-win32-x86-stripped.jar
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt eclipse\swt\lib\swt-3.5-win32-win32-x86_64.jar eclipse\swt\lib\swt-3.5-win32-win32-x86_64-stripped.jar
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt org\json\090819\json-090819.jar org\json\090819\json-090819-stripped.jar
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt jcommon\jcommon\1.0.16\jcommon-1.0.16.jar jcommon\jcommon\1.0.16\jcommon-1.0.16-stripped.jar 
-java -jar jarjar\jarjar\lib\jarjar-1.0.jar process strip-rules.txt jfreechart\jfreechart\1.0.13\jfreechart-1.0.13.jar jfreechart\jfreechart\1.0.13\jfreechart-1.0.13-stripped.jar 
-
