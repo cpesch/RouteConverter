01ba372beb38d545d5c43f70c8b65d6fa648c5ca
Christian Pesch <christian_pesch@gmx.de>
2010-01-24 14:47:06 +0000

    do hex en/decoding with commons-codec
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@1616 964dd122-5732-0410-82e6-8cc08e73365a

--- common/src/main/java/slash/common/hex/HexDecoder.java
+++ common/src/main/java/slash/common/hex/HexDecoder.java
@@ -20,108 +20,21 @@
 
 package slash.common.hex;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.Writer;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 /**
- * Helps to decode hexadecimally encoded digits to bytes, shorts,
- * ints or a hexadecimally encoded Writer to an OutputStream.
+ * Helps to decode hexadecimally encoded strings to bytes.
  *
  * @author Christian Pesch
  */
 
-public class HexDecoder extends Writer {
-    private final OutputStream outputStream;
-
-    public HexDecoder(OutputStream stream) {
-        this.outputStream = stream;
-    }
-
-    public void write(String stringified, int from, int length) throws IOException {
-        byte[] result = new byte[length / 2];
-
-        for (int i = 0; i < result.length; i++) {
-            result[i] = (byte) ((decoding[stringified.charAt(from + 2 * i)] << 4) +
-                    decoding[stringified.charAt(from + 1 + 2 * i)]);
-        }
-        outputStream.write(result);
-    }
-
-    public void write(char[] stringified, int from, int length) throws IOException {
-        write(new String(stringified), from, length);
-    }
-
-    public void flush() throws IOException {
-        outputStream.flush();
-    }
-
-    public void close() throws IOException {
-        outputStream.close();
-    }
-
-    /**
-     * Decode the given string to a byte array. Requires, that the string is a
-     * sequence of hex digits of even length.
-     *
-     * @param string the hex string to be decoded
-     * @return the decoded bytes, where result[i] is computed from string.charAt(2*i) and string.charAt(2*i+1)
-     * @see Character#digit
-     */
+public class HexDecoder {
     public static byte[] decodeBytes(String string) {
-        if (string.length() % 2 != 0)
+        try {
+            return Hex.decodeHex(string.toCharArray());
+        } catch (DecoderException e) {
             throw new IllegalArgumentException("Not an even number of hex digits: " + string);
-        byte[] result = new byte[string.length() / 2];
-        for (int i = 0; i < result.length; ++i) {
-            int highNibble = Character.digit(string.charAt(2 * i), 16);
-            int lowNibble = Character.digit(string.charAt(2 * i + 1), 16);
-            if (highNibble == -1 || lowNibble == -1)
-                throw new IllegalArgumentException("Not a string of hex digits: " + string);
-            result[i] = (byte) (highNibble * 16 + lowNibble);
         }
-        return result;
-    }
-
-
-    private static final byte[] decoding = new byte['g'];
-    static {
-        decoding['0'] = 0;
-        decoding['1'] = 1;
-        decoding['2'] = 2;
-        decoding['3'] = 3;
-        decoding['4'] = 4;
-        decoding['5'] = 5;
-        decoding['6'] = 6;
-        decoding['7'] = 7;
-        decoding['8'] = 8;
-        decoding['9'] = 9;
-        decoding['a'] = 10;
-        decoding['b'] = 11;
-        decoding['c'] = 12;
-        decoding['d'] = 13;
-        decoding['e'] = 14;
-        decoding['f'] = 15;
-        decoding['A'] = 10;
-        decoding['B'] = 11;
-        decoding['C'] = 12;
-        decoding['D'] = 13;
-        decoding['E'] = 14;
-        decoding['F'] = 15;
-    }
-
-    private static byte decodeByte(String string, int pos) throws IndexOutOfBoundsException {
-        return (byte) ((decoding[string.charAt(pos)] << 4) + decoding[string.charAt(pos + 1)]);
-    }
-
-    private static short decodeShort(String string, int pos) throws IndexOutOfBoundsException {
-        return (short) ((decoding[string.charAt(pos)] << 12) + (decoding[string.charAt(pos + 1)] << 8) +
-                (decoding[string.charAt(pos + 2)] << 4) + decoding[string.charAt(pos + 3)]);
-    }
-
-    private static int decodeInt(String string, int pos) throws IndexOutOfBoundsException {
-        return ((decoding[string.charAt(pos)] << 28) + (decoding[string.charAt(pos + 1)] << 24) +
-                (decoding[string.charAt(pos + 2)] << 20) + (decoding[string.charAt(pos + 3)] << 16) +
-                (decoding[string.charAt(pos) + 4] << 12) + (decoding[string.charAt(pos + 5)] << 8) +
-                (decoding[string.charAt(pos + 6)] << 4) + decoding[string.charAt(pos + 7)]);
     }
 }
--- common/src/main/java/slash/common/hex/HexEncoder.java
+++ common/src/main/java/slash/common/hex/HexEncoder.java
@@ -20,111 +20,16 @@
 
 package slash.common.hex;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.StringWriter;
-import java.io.Writer;
+import org.apache.commons.codec.binary.Hex;
 
 /**
- * Helps to encode bytes, shorts, ints to hexadecimal encoded
- * digits or an Outputstream to an encoded Writer.
+ * Helps to encode bytes to hexadecimal encoded digits.
  *
  * @author Christian Pesch
  */
 
-public class HexEncoder extends OutputStream {
-    private final Writer writer;
-
-    private HexEncoder(Writer writer) {
-        this.writer = writer;
-    }
-
-    public void write(int anInt) throws IOException {
-        writer.write(lowByte(anInt));
-        writer.write(highByte(anInt));
-    }
-
-    public void flush() throws IOException {
-        writer.flush();
-    }
-
-    public void close() throws IOException {
-        writer.close();
-    }
-
-
-    private static final char[] encoding = new char[16];
-    static {
-        encoding[0] = '0';
-        encoding[1] = '1';
-        encoding[2] = '2';
-        encoding[3] = '3';
-        encoding[4] = '4';
-        encoding[5] = '5';
-        encoding[6] = '6';
-        encoding[7] = '7';
-        encoding[8] = '8';
-        encoding[9] = '9';
-        encoding[10] = 'A';
-        encoding[11] = 'B';
-        encoding[12] = 'C';
-        encoding[13] = 'D';
-        encoding[14] = 'E';
-        encoding[15] = 'F';
-    }
-
-    private static char lowByte(int aByte) {
-        return encoding[((aByte >> 4) & 0x0f)];
-    }
-
-    private static char highByte(int aByte) {
-        return encoding[(aByte & 0x0f)];
-    }
-
+public class HexEncoder {
     public static String encodeByte(byte aByte) {
-        char[] chars = new char[2];
-        chars[0] = lowByte(aByte);
-        chars[1] = highByte(aByte);
-        return new String(chars);
-    }
-
-    public static String encodeBytes(byte[] bytes) {
-        StringWriter writer = new StringWriter(2 * bytes.length);
-        HexEncoder encoder = new HexEncoder(writer);
-        try {
-            encoder.write(bytes);
-            encoder.close();
-        } catch (IOException e) {
-            throw new RuntimeException("No IOException is possible here");
-        }
-        return writer.toString();
-    }
-
-    private static String encodeShort(short aShort) {
-        return encodeByte((byte) ((aShort >> 8) & 0x00ff)) +
-                encodeByte((byte) (aShort & 0x00ff));
-    }
-
-    private static String encodeInt(int anInt) {
-        return encodeShort((short) ((anInt >> 16) & 0x0000ffff)) +
-                encodeShort((short) (anInt & 0x0000ffff));
-    }
-
-    private static String encodeLong(long aLong) {
-        return encodeInt((int) ((aLong >> 32))) +
-                encodeInt((int) (aLong));
-    }
-
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println(HexEncoder.class + " <int1> [<int2> ...]");
-            System.exit(1);
-        }
-
-        for (String arg : args) {
-            int decimal = Integer.parseInt(arg);
-            System.out.println("Dec " + decimal + " is hex " + HexEncoder.encodeInt(decimal));
-        }
-        System.exit(0);
+        return new String(Hex.encodeHex(new byte[]{aByte})).toUpperCase();
     }
 }
