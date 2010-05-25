87ff930f0349ea9b2c95519e437cdf70b4be69e5
Christian Pesch <christian_pesch@gmx.de>
2010-05-25 05:40:36 +0000

    added earthtools height webservice access
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@1743 964dd122-5732-0410-82e6-8cc08e73365a

---
+++ earthtools/src/main/java/slash/navigation/earthtools/EarthToolsService.java
@@ -0,0 +1,60 @@
+/*
+    This file is part of RouteConverter.
+
+    RouteConverter is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    RouteConverter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RouteConverter; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright (C) 2007 Christian Pesch. All Rights Reserved.
+*/
+
+package slash.navigation.earthtools;
+
+import slash.common.io.Transfer;
+import slash.navigation.earthtools.binding.Height;
+import slash.navigation.rest.Get;
+
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+/**
+ * Encapsulates REST access to the earthtools.org service.
+ *
+ * @author Christian Pesch
+ */
+
+public class EarthToolsService {
+    private static final Preferences preferences = Preferences.userNodeForPackage(EarthToolsService.class);
+    private static final String GEONAMES_URL_PREFERENCE = "earthtoolsUrl";
+
+    private static String getEarthToolsUrlPreference() {
+        return preferences.get(GEONAMES_URL_PREFERENCE, "http://www.earthtools.org/");
+    }
+
+    public Integer getElevationFor(double longitude, double latitude) throws IOException {
+        Get get = new Get(getEarthToolsUrlPreference() + "height/" + latitude + "/" + longitude);
+        String result = get.execute();
+        if (get.isSuccessful())
+            try {
+                Height height = EarthToolsUtil.unmarshal(result);
+                Integer elevation = Transfer.parseInt(height.getMeters());
+                if (elevation != null && !elevation.equals(-9999))
+                    return elevation;
+            } catch (Exception e) {
+                IOException io = new IOException("Cannot unmarshall " + result + ": " + e.getMessage());
+                io.setStackTrace(e.getStackTrace());
+                throw io;
+            }
+        return null;
+    }
+}
