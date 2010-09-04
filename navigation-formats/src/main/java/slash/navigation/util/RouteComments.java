1ba23f6e64df2b4a865cb264fe3acd5d90474718
Christian Pesch <christian_pesch@gmx.de>
2010-09-04 11:06:16 +0000

    classical boolean fail
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@1979 964dd122-5732-0410-82e6-8cc08e73365a

--- navigation-formats/src/main/java/slash/navigation/util/RouteComments.java
+++ navigation-formats/src/main/java/slash/navigation/util/RouteComments.java
@@ -298,7 +298,7 @@ public abstract class RouteComments {
 
     private static Double parseDouble(String string) {
         Double aDouble = Transfer.parseDouble(string);
-        return Transfer.isEmpty(aDouble) ? aDouble : null;
+        return !Transfer.isEmpty(aDouble) ? aDouble : null;
     }
 
     public static void parseComment(BaseNavigationPosition position, String comment) {
