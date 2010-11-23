70669ee554c8fd6687888be1df6c229fd9b504bf
Christian Pesch <christian_pesch@gmx.de>
2010-11-23 20:43:36 +0000

    move to JUnit4 store all comments for further analyze steps
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@2119 964dd122-5732-0410-82e6-8cc08e73365a

--- navigation-formats/src/test/java/slash/navigation/base/ReadIT.java
+++ navigation-formats/src/test/java/slash/navigation/base/ReadIT.java
@@ -20,15 +20,24 @@
 
 package slash.navigation.base;
 
+import org.junit.AfterClass;
+import org.junit.Test;
 import slash.common.io.Files;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class ReadIT extends NavigationTestCase {
-    NavigationFileParser parser = new NavigationFileParser();
+import static slash.navigation.base.NavigationTestCase.*;
+
+public class ReadIT {
+    private NavigationFileParser parser = new NavigationFileParser();
+    private static Set<String> comments = new HashSet<String>();
 
     protected interface TestFileCallback {
         void test(File file) throws IOException;
@@ -49,12 +58,19 @@ public class ReadIT extends NavigationTestCase {
 
     private void readFiles(String extension) throws IOException {
         readFiles(extension, new TestFileCallback() {
+            @SuppressWarnings({"unchecked"})
             public void test(File file) throws IOException {
                 assertTrue("Cannot read route from " + file, parser.read(file));
                 assertNotNull(parser.getFormat());
                 assertNotNull("Cannot get route from " + file, parser.getTheRoute());
                 assertNotNull(parser.getAllRoutes());
                 assertTrue(parser.getAllRoutes().size() > 0);
+                for(BaseRoute route : parser.getAllRoutes()) {
+                    List<BaseNavigationPosition> positions = route.getPositions();
+                    for(BaseNavigationPosition position : positions) {
+                        comments.add(position.getComment());
+                    }
+                }
                 if (!NO_NAME_DEFINED.contains(file.getName()) && !file.getName().endsWith(".axe") &&
                         !file.getName().endsWith(".gdb") && !file.getName().endsWith(".nmea") &&
                         !file.getName().endsWith(".mps") && !file.getName().endsWith(".rte") &&
@@ -68,109 +84,147 @@ public class ReadIT extends NavigationTestCase {
         });
     }
 
+    @AfterClass
+    public static void tearDown() throws IOException {
+        PrintStream out = new PrintStream(new FileOutputStream(File.createTempFile("comments", ".csv")));
+        String[] strings = comments.toArray(new String[comments.size()]);
+        Arrays.sort(strings);
+        for (String string : strings) {
+            out.println(string);
+        }
+        out.flush();
+        out.close();
+    }
+
+    @Test
     public void testAscFilesAreValid() throws IOException {
         readFiles(".asc");
     }
 
+    @Test
     public void testMicrosoftAutoRouteFilesAreValid() throws IOException {
         readFiles(".axe");
     }
 
+    @Test
     public void testMicrosoftMapPointFilesAreValid() throws IOException {
         readFiles(".ptm");
     }
 
+    @Test
     public void testBcrFilesAreValid() throws IOException {
         readFiles(".bcr");
     }
 
+    @Test
     public void testCsvFilesAreValid() throws IOException {
         // Columbus V900
         readFiles(".csv");
     }
 
+    @Test
     public void testGarminPoiFilesAreValid() throws IOException {
         readFiles(".gpi");
     }
 
+    @Test
     public void testGarminPoiDbFilesAreValid() throws IOException {
         readFiles(".xcsv");
     }
 
+    @Test
     public void testGarminMapSource6FilesAreValid() throws IOException {
         readFiles(".gdb");
     }
 
+    @Test
     public void testGeoCachingFilesAreValid() throws IOException {
         readFiles(".loc");
     }
 
+    @Test
     public void testGpxFilesAreValid() throws IOException {
         readFiles(".gpx");
     }
 
+    @Test
     public void testHoluxM241BinaryFilesAreValid() throws IOException {
         readFiles(".bin");
     }
 
+    @Test
     public void testTomTomRouteFilesAreValid() throws IOException {
         readFiles(".itn");
     }
 
+    @Test
     public void testKmlFilesAreValid() throws IOException {
         readFiles(".kml");
     }
 
+    @Test
     public void testKmzFilesAreValid() throws IOException {
         readFiles(".kmz");
     }
 
+    @Test
     public void testKlickTelRouteFilesAreValid() throws IOException {
         readFiles(".krt");
     }
 
+    @Test
     public void testMagellanExploristFilesAreValid() throws IOException {
         readFiles(".log");
     }
 
+    @Test
     public void testMagicMapsIktFilesAreValid() throws IOException {
         readFiles(".ikt");
     }
 
+    @Test
     public void testNokiaLandmarkExchangeFilesAreValid() throws IOException {
         readFiles(".lmx");
     }
 
+    @Test
     public void testNmeaFilesAreValid() throws IOException {
         readFiles(".nmea");
         readFiles(".pgl");
     }
 
+    @Test
     public void testNmn7FilesAreValid() throws IOException {
         readFiles(".freshroute");
     }
 
+    @Test
     public void testMagellanMapSendFilesAreValid() throws Exception {
-        Thread.sleep(5000); // this seems to help against the errors that only show up on complete runs
+        // TODO Thread.sleep(5000); // this seems to help against the errors that only show up on complete runs
         readFiles(".mps");
     }
 
+    @Test
     public void testOv2FilesAreValid() throws IOException {
         readFiles(".ov2");
     }
 
+    @Test
     public void testOvlFilesAreValid() throws IOException {
         readFiles(".ovl");
     }
 
+    @Test
     public void testOziExplorerTrackFilesAreValid() throws IOException {
         readFiles(".plt");
     }
 
+    @Test
     public void testMagicMapsPthFilesAreValid() throws IOException {
         readFiles(".pth");
     }
 
+    @Test
     public void testRteFilesAreValid() throws IOException {
         // Magellan Route
         // Navigon Mobile Navigator
@@ -178,42 +232,51 @@ public class ReadIT extends NavigationTestCase {
         readFiles(".rte");
     }
 
+    @Test
     public void testNavilinkFilesAreValid() throws IOException {
         readFiles(".sbp");
     }
 
+    @Test
     public void testTourExchangeFilesAreValid() throws IOException {
         readFiles(".tef");
     }
 
+    @Test
     public void testTrainingCenterDatabaseFilesAreValid() throws IOException {
         readFiles(".crs");
         readFiles(".hst");
         readFiles(".tcx");
     }
 
+    @Test
     public void testTourFilesAreValid() throws IOException {
         readFiles(".tour");
     }
 
+    @Test
     public void testTrkFilesAreValid() throws IOException {
         readFiles(".trk");
     }
 
+    @Test
     public void testTkFilesAreValid() throws IOException {
         // Glopus
         // Kompass
         readFiles(".tk");
     }
 
+    @Test
     public void testNationalGeographicTopo3FilesAreValid() throws IOException {
         readFiles(".tpo");
     }
 
+    @Test
     public void testTrpFilesAreValid() throws IOException {
         readFiles(".trp");
     }
 
+    @Test
     public void testTextFilesAreValid() throws IOException {
         // KienzleGps
         // MagicMaps2Go
@@ -221,39 +284,48 @@ public class ReadIT extends NavigationTestCase {
         readFiles(".txt");
     }
 
+    @Test
     public void testAlanTracklogFilesAreValid() throws IOException {
         readFiles(".trl");
     }
 
+    @Test
     public void testUrlFilesAreValid() throws IOException {
         readFiles(".url");
     }
 
+    @Test
     public void testWintecWbt202TesFilesAreValid() throws IOException {
         readFiles(".tes");
     }
 
+    @Test
     public void testWintecWbt201Tk1FilesAreValid() throws IOException {
         readFiles(".tk1");
     }
 
+    @Test
     public void testWintecWbt201Tk2FilesAreValid() throws IOException {
         readFiles(".tk2");
     }
 
+    @Test
     public void testWprFilesAreValid() throws IOException {
         readFiles(".wpr");
     }
 
+    @Test
     public void testOziExplorerWaypointFilesAreValid() throws IOException {
         readFiles(".wpt");
     }
 
+    @Test
     public void testXmlFilesAreValid() throws IOException {
         // GoPal 3,5
         readFiles(".xml");
     }
 
+    @Test
     public void testViaMichelinFilesAreValid() throws IOException {
         readFiles(".xvm");
     }
@@ -269,6 +341,7 @@ public class ReadIT extends NavigationTestCase {
     public static final String UNRECOGNIZED_PATH = ROUTE_PATH + "unrecognized\\";
     public static final String FALSE_DETECTS_PATH = ROUTE_PATH + "falsedetects\\";
 
+    @Test
     public void testDontReadUnrecognizedFiles() throws IOException {
         dontReadFiles(UNRECOGNIZED_PATH, new TestFileCallback() {
             public void test(File file) throws IOException {
@@ -279,6 +352,7 @@ public class ReadIT extends NavigationTestCase {
         });
     }
 
+    @Test
     public void testReadFalseDetectsFiles() throws IOException {
         dontReadFiles(FALSE_DETECTS_PATH, new TestFileCallback() {
             public void test(File file) throws IOException {
