4bec65ec62f6d2cad0342e9634ba6311bf707d1c
Christian Pesch <christian_pesch@gmx.de>
2010-12-20 09:17:55 +0000

    catch and log exception
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@2155 964dd122-5732-0410-82e6-8cc08e73365a

--- route-converter/src/main/java/slash/navigation/converter/gui/dnd/ClipboardInteractor.java
+++ route-converter/src/main/java/slash/navigation/converter/gui/dnd/ClipboardInteractor.java
@@ -4,9 +4,16 @@ import slash.navigation.gui.Application;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.FlavorEvent;
+import java.awt.datatransfer.FlavorListener;
+import java.awt.datatransfer.Transferable;
+import java.util.logging.Logger;
 
 public class ClipboardInteractor {
+    private static final Logger log = Logger.getLogger(ClipboardInteractor.class.getName());
 
     private void enable(boolean enable) {
         Application.getInstance().getContext().getActionManager().enable("paste", enable);
@@ -21,9 +28,14 @@ public class ClipboardInteractor {
     }
 
     private boolean isSupportedFlavor() {
-        for (DataFlavor f : Toolkit.getDefaultToolkit().getSystemClipboard().getAvailableDataFlavors()) {
-            if (f.equals(PositionSelection.positionFlavor) || f.equals(PositionSelection.stringFlavor))
-                return true;
+        try {
+            for (DataFlavor f : Toolkit.getDefaultToolkit().getSystemClipboard().getAvailableDataFlavors()) {
+                if (f.equals(PositionSelection.positionFlavor) || f.equals(PositionSelection.stringFlavor))
+                    return true;
+            }
+        }
+        catch (IllegalStateException e) {
+            log.warning("Cannot get available data flavors: " + e.getMessage());
         }
         return false;
     }
