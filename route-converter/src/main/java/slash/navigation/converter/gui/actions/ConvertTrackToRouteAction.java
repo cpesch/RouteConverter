45151bb84c042d55370e59a50aa995e77f3cb13c
Christian Pesch <christian_pesch@gmx.de>
2010-12-21 14:24:22 +0000

    add actions for simple usage of complex functions
    git-svn-id: http://www.routeconverter.de/svn/RouteConverter/trunk@2158 964dd122-5732-0410-82e6-8cc08e73365a

---
+++ route-converter/src/main/java/slash/navigation/converter/gui/actions/ConvertTrackToRouteAction.java
@@ -0,0 +1,41 @@
+/*
+    This file is part of RouteConverter.
+
+    RouteConverter is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    RouteConverter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RouteConverter; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright (C) 2007 Christian Pesch. All Rights Reserved.
+*/
+
+package slash.navigation.converter.gui.actions;
+
+import slash.navigation.converter.gui.RouteConverter;
+import slash.navigation.gui.FrameAction;
+
+import static slash.navigation.base.RouteCharacteristics.Route;
+
+/**
+ * Convert the current track position list into a route.
+ *
+ * @author Christian Pesch
+ */
+
+public class ConvertTrackToRouteAction extends FrameAction {
+    public void run() {
+        RouteConverter r = RouteConverter.getInstance();
+        r.setRouteCharacteristics(Route);
+        r.selectInsignificantPositions(100);
+        r.getContext().getActionManager().run("delete");
+    }
+}
