5388c7c0d5a128791811089ad1b80ac984cf19ba
Christian Pesch <Christian_Pesch@gmx.de>
2011-01-20 15:24:20 +0100

    provide test file examples and mechanism to access my test file database

--- navigation-formats/src/test/java/slash/navigation/base/NavigationTestCase.java
+++ navigation-formats/src/test/java/slash/navigation/base/NavigationTestCase.java
@@ -65,7 +65,7 @@ import java.util.logging.Logger;
 public abstract class NavigationTestCase extends TestCase {
     protected static final Logger log = Logger.getLogger(NavigationTestCase.class.getName());
 
-    public static final String ROUTE_PATH = "routes" + File.separator + "src" + File.separator;
+    public static final String ROUTE_PATH = System.getProperty("samples", "routes" + File.separator + "src" + File.separator);
     public static final String TEST_PATH = ROUTE_PATH + "test" + File.separator;
     public static final String SAMPLE_PATH = ROUTE_PATH + "samples" + File.separator;
 
