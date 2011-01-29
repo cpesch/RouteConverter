86c2ec029376e39fb77d71fe88045e3960308607
Christian Pesch <Christian_Pesch@gmx.de>
2011-01-29 18:30:48 +0100

    recalculate track length after change of track position lists - might degrade performance :-(

--- route-converter/src/main/java/slash/navigation/converter/gui/helper/LengthCalculator.java
+++ route-converter/src/main/java/slash/navigation/converter/gui/helper/LengthCalculator.java
@@ -67,8 +67,12 @@ public class LengthCalculator {
                 // ignored updates on columns not relevant for length calculation
                 if (e.getType() == TableModelEvent.UPDATE &&
                         !(e.getColumn() == PositionColumns.LONGITUDE_COLUMN_INDEX ||
-                                e.getColumn() == PositionColumns.LATITUDE_COLUMN_INDEX))
+                                e.getColumn() == PositionColumns.LATITUDE_COLUMN_INDEX ||
+                                e.getColumn() == TableModelEvent.ALL_COLUMNS))
                     return;
+
+                if (e.getColumn() == TableModelEvent.ALL_COLUMNS)
+                    log.info("Calculating length after all columns were updated"); // TODO might point to performance degration
 
                 calculateDistance();
             }
