47a19b8a3a507bf4aee49b9f3e79186db9fec252
Christian Pesch <Christian_Pesch@gmx.de>
2011-05-15 20:23:03 +0200

    reformat comments

--- hgt/src/main/java/slash/navigation/hgt/ElevationTile.java
+++ hgt/src/main/java/slash/navigation/hgt/ElevationTile.java
@@ -58,7 +58,7 @@ public class ElevationTile {
         double dLat = latitude;
         int nLon = (int) dLon;                    // Cut off the decimal places
         int nLat = (int) dLat;                    // Cut off the decimal places
-        int nAS = 1200;                            // 1200 Intervals (means 1201 positions per line and column)
+        int nAS = 1200;                           // 1200 Intervals (means 1201 positions per line and column)
 
         if (dLon < 0) {                                        // If it's west longitude (negative value)
             nLon = (nLon - 1) * -1;                            // Make a positive number (left edge)
@@ -67,7 +67,7 @@ public class ElevationTile {
 
         if (dLat < 0) {                                        // If it's a south latitude (negative value)
             nLat = (nLat - 1) * -1;                            // Make a positive number (bottom edge)
-            dLat = ((double) nLat + dLat) + (double) nLat;    // Make positive double latitude (needed for later calculation)
+            dLat = ((double) nLat + dLat) + (double) nLat;     // Make positive double latitude (needed for later calculation)
         }
 
         int nLonIndex = (int) ((dLon - (double) nLon) * 1200.0); // Calculate the interval index for longitude
@@ -85,42 +85,42 @@ public class ElevationTile {
         double dOffLat = dLat - (double) nLat;                      // The lat value offset within a tile
 
         double dLeftTop;                                            // The left top position of a sub tile
-        double dLeftBottom;                                            // The left bottom position of a sub tile
-        double dRightTop;                                            // The right top position of a sub tile
+        double dLeftBottom;                                         // The left bottom position of a sub tile
+        double dRightTop;                                           // The right top position of a sub tile
         double dRightBottom;                                        // The right bootm position of a sub tile
         int pos;                                                    // The index of the elevation into the hgt file
 
         pos = (((nAS - nLatIndex) - 1) * (nAS + 1)) + nLonIndex;    // The index for the left top elevation
-        elevationFile.seek(pos * 2);                            // We have 16-bit values for elevation, so multiply by 2
-        dLeftTop = elevationFile.readShort();                // Now read the left top elevation from hgt file
+        elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
+        dLeftTop = elevationFile.readShort();                       // Now read the left top elevation from hgt file
 
-        pos = ((nAS - nLatIndex) * (nAS + 1)) + nLonIndex;            // The index for the left bottom elevation
-        elevationFile.seek(pos * 2);                            // We have 16-bit values for elevation, so multiply by 2
-        dLeftBottom = elevationFile.readShort();                // Now read the left bottom elevation from hgt file
+        pos = ((nAS - nLatIndex) * (nAS + 1)) + nLonIndex;          // The index for the left bottom elevation
+        elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
+        dLeftBottom = elevationFile.readShort();                    // Now read the left bottom elevation from hgt file
 
         pos = (((nAS - nLatIndex) - 1) * (nAS + 1)) + nLonIndex + 1;// The index for the right top elevation
-        elevationFile.seek(pos * 2);                            // We have 16-bit values for elevation, so multiply by 2
-        dRightTop = elevationFile.readShort();                // Now read the right top elevation from hgt file
+        elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
+        dRightTop = elevationFile.readShort();                      // Now read the right top elevation from hgt file
 
-        pos = ((nAS - nLatIndex) * (nAS + 1)) + nLonIndex + 1;         // The index for the right bottom elevation
-        elevationFile.seek(pos * 2);                            // We have 16-bit values for elevation, so multiply by 2
-        dRightBottom = elevationFile.readShort();                // Now read the right bottom top elevation from hgt file
+        pos = ((nAS - nLatIndex) * (nAS + 1)) + nLonIndex + 1;      // The index for the right bottom elevation
+        elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
+        dRightBottom = elevationFile.readShort();                   // Now read the right bottom top elevation from hgt file
 
-        if ((dLeftTop < 0) ||                                        // If one of the elevation values
-                (dLeftBottom < 0) ||                                    // we read from
-                (dRightTop < 0) ||                                        // the hgt file is
-                (dRightBottom < 0)) {                                    // not valid
+        if ((dLeftTop < 0) ||                                       // If one of the elevation values
+                (dLeftBottom < 0) ||                                // we read from
+                (dRightTop < 0) ||                                  // the hgt file is
+                (dRightBottom < 0)) {                               // not valid
             return null;                                            // we can't interpolate
         }
 
         double dDeltaLat;         // The delta between top lat value and wanted lat (delta within a sub tile)
-        double dDeltaLon;       // The delta between left lon value and wanted lon (delta within a sub tile)
+        double dDeltaLon;         // The delta between left lon value and wanted lon (delta within a sub tile)
 
         dDeltaLon = dOffLon - (double) nLonIndex * (1.0 / (double) nAS);   // The delta (offset) from left point to wanted point
         dDeltaLat = dOffLat - (double) nLatIndex * (1.0 / (double) nAS);   // The delta (offset) from bottom point to wanted point
 
         double dLonHeightLeft;    // The interpolated elevation calculated from left top to left bottom
-        double dLonHeightRight;    // The interpolated elevation calculated from right top to right bottom
+        double dLonHeightRight;   // The interpolated elevation calculated from right top to right bottom
 
         dLonHeightLeft = dLeftBottom - calculateElevation(dLeftBottom - dLeftTop, 1.0 / (double) nAS, dDeltaLat);
         dLonHeightRight = dRightBottom - calculateElevation(dRightBottom - dRightTop, 1.0 / (double) nAS, dDeltaLat);
@@ -128,6 +128,6 @@ public class ElevationTile {
         // Interpolate between the interpolated left elevation and interpolated right elevation
         dElevation = dLonHeightLeft - calculateElevation(dLonHeightLeft - dLonHeightRight, 1.0 / (double) nAS, dDeltaLon);
 
-        return (int) (dElevation + 0.5);        // Do a rounding of the calculated elevation
+        return (int) (dElevation + 0.5);   // Do a rounding of the calculated elevation
     }
 }
