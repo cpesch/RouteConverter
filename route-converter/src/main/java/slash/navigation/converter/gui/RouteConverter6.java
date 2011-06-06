2a9caf41062cdcc3e5bf4f2d44560f00777b5198
Christian Pesch <Christian_Pesch@gmx.de>
2011-06-06 12:52:09 +0200

    Workaround for the forever caching JNLP problem

---
+++ route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter6.java
@@ -0,0 +1,9 @@
+package slash.navigation.converter.gui;
+
+/** Workaround for the forever caching JNLP problem */
+
+public class RouteConverter6 extends RouteConverter {
+    public static void main(String[] args) {
+        launch(RouteConverter.class, args);
+    }
+}
