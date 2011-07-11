678b57e50a2bf9266c55abd811bd6fb5eebfb605
Christian Pesch <Christian_Pesch@gmx.de>
2011-07-11 22:02:03 +0200

    now reads all broken Navilink (*.sbp) files

--- navigation-formats/src/main/java/slash/navigation/simple/BrokenNavilinkFormat.java
+++ navigation-formats/src/main/java/slash/navigation/simple/BrokenNavilinkFormat.java
@@ -24,15 +24,11 @@ package slash.navigation.simple;
 import slash.common.io.CompactCalendar;
 import slash.navigation.base.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -42,54 +38,112 @@ import java.util.List;
  */
 
 public class BrokenNavilinkFormat extends NavilinkFormat {
-    private static final int SBP_RECORD_LENGTH_SHORT = 31;
+    private static final long MINIMUM_TIME_MILLISECONDS = 949427965809l; // 01.01.2000
 
     public String getName() {
         return "Navilink Garble (*" + getExtension() + ")";
     }
 
-    public List<Wgs84Route> read(InputStream source, CompactCalendar startDate) throws IOException {
-        List<Wgs84Route> resultRouteList = null;
+    private int readOneByteFromInput(InputStream source, byte[] record) throws IOException {
+        System.arraycopy(record, 1, record, 0, record.length - 1);
+        return source.read(record, SBP_RECORD_LENGTH - 1, 1);
+    }
 
-        byte[] header = new byte[HEADER_SIZE];
-        if (source.read(header) == HEADER_SIZE) {
-            ByteBuffer sbpRecordByteBuffer = ByteBuffer.allocate(SBP_RECORD_LENGTH);
-            sbpRecordByteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+    private boolean isValidPosition(Wgs84Position position, Wgs84Position previous) {
+        boolean valid = (position.getHdop() > -0.0000000001 &&
+                position.getHdop() < 10.21);
+        valid = valid && (position.getLatitude() >= -90 &&
+                position.getLatitude() <= 90);
+        valid = valid && (position.getLongitude() >= -180 &&
+                position.getLatitude() <= 180);
+        valid = valid && (position.getElevation() > -100 &&
+                position.getElevation() < 14000);
+        valid = valid && (position.getSpeed() >= -0.9999999 &&
+                position.getSpeed() < 1200);
+        valid = valid && (position.getHeading() > -0.999999 &&
+                position.getHeading() < 360.000001);
+        valid = valid && (position.getTime().getTimeInMillis() > MINIMUM_TIME_MILLISECONDS &&
+                position.getTime().getTimeInMillis() < System.currentTimeMillis());
 
-            resultRouteList = new ArrayList<Wgs84Route>();
-            Wgs84Route activeRoute = null;
-
-            byte[] record = new byte[SBP_RECORD_LENGTH];
-            Wgs84Position position;
-            int expectedByteLength = SBP_RECORD_LENGTH;
-            while (source.read(record, 0, expectedByteLength) == expectedByteLength) {
-                if (expectedByteLength == SBP_RECORD_LENGTH_SHORT) {
-                    // insert 1 byte at position 1
-                    for (int i = record.length - 1; i > 1; i--)
-                        record[i] = record[i - 1];
-                    record[1] = 3;
-
-                    expectedByteLength = SBP_RECORD_LENGTH;
-                }
-
-                sbpRecordByteBuffer.position(0);
-                sbpRecordByteBuffer.put(record);
-
-                position = decodePosition(sbpRecordByteBuffer);
-                if ((short) record[30] > 0x0F) {
-                    expectedByteLength = SBP_RECORD_LENGTH_SHORT;
-                }
-
-                if ((activeRoute == null) || (isTrackStart(sbpRecordByteBuffer))) {
-                    activeRoute = createRoute(RouteCharacteristics.Track,
-                            TRACK_NAME_DATE_FORMAT.format(position.getTime().getTime()),
-                            new ArrayList<BaseNavigationPosition>());
-                    resultRouteList.add(activeRoute);
-                }
-
-                activeRoute.getPositions().add(position);
-            }
+        if (previous != null) {
+            // valid = valid && (position.getTime().getTimeInMillis() > previous.getTime().getTimeInMillis());
+            // would ignore too much points, thus I've build an after read check function
+            Double speed = position.calculateSpeed(previous);
+            if (speed != null)
+                valid = valid && (speed < 1200);
         }
-        return resultRouteList;
+
+        return valid;
+    }
+
+    private void deleteLogicalWrongPositionsInList(List<Wgs84Position> positions) {
+        int i = 1;
+        while (i < positions.size() - 1) {
+            Wgs84Position prev = positions.get(i - 1);
+            Wgs84Position actual = positions.get(i);
+            Wgs84Position next = positions.get(i + 1);
+
+            if (prev.getTime().getTimeInMillis() < actual.getTime().getTimeInMillis() &&
+                    actual.getTime().getTimeInMillis() < next.getTime().getTimeInMillis())
+                i++;
+            else
+                positions.remove(i);
+        }
+    }
+
+    private void deleteLogicalWrongPositions(List<Wgs84Route> routeList) {
+        for (Wgs84Route route : routeList) {
+            deleteLogicalWrongPositionsInList(route.getPositions());
+        }
+    }
+
+    public List<Wgs84Route> read(InputStream source, CompactCalendar startDate) throws IOException {
+        byte[] record = new byte[SBP_RECORD_LENGTH];
+        ByteBuffer sbpRecordByteBuffer = ByteBuffer.wrap(record);
+        sbpRecordByteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        List<Wgs84Route> result = new ArrayList<Wgs84Route>();
+        Wgs84Route activeRoute = null;
+
+        Wgs84Position position;
+        Wgs84Position previousPosition = null;
+
+        int readBytes = 0, pointCount = 0;
+        while (source.read(record) == SBP_RECORD_LENGTH) {
+            do {
+                sbpRecordByteBuffer.position(0);
+                position = decodePosition(sbpRecordByteBuffer);
+                if (!isValidPosition(position, previousPosition)) {
+                    position = null;
+                    int count = readOneByteFromInput(source, record);
+                    readBytes += count;
+                    if (count != 1) {
+                        break;
+                    }
+                }
+            } while (position == null);
+
+            // is format correct?
+            if (readBytes > 100 && pointCount < 3) {
+                return null;
+            }
+
+            if ((activeRoute == null || isTrackStart(sbpRecordByteBuffer)) && position != null) {
+                activeRoute = createRoute(RouteCharacteristics.Track,
+                        TRACK_NAME_DATE_FORMAT.format(position.getTime().getTime()),
+                        new ArrayList<BaseNavigationPosition>());
+                result.add(activeRoute);
+            }
+
+            if ((position != null) && (activeRoute != null))
+                activeRoute.getPositions().add(position);
+            else
+                return result;
+
+            pointCount++;
+            previousPosition = position;
+        }
+        deleteLogicalWrongPositions(result);
+        return result;
     }
 }
