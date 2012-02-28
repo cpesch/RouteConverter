5e7df5c83ba7291ff742211093a4656b978ac4c7
Christian Pesch <Christian_Pesch@gmx.de>
2012-02-28 22:56:20 +0100

    not needed anymore after all that Webstart hassle

--- route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter6.java
+++
@@ -1,9 +0,0 @@
-package slash.navigation.converter.gui;
-
-/** Workaround for the forever caching JNLP problem */
-
-public class RouteConverter6 extends RouteConverter {
-    public static void main(String[] args) {
-        launch(RouteConverter.class, args);
-    }
-}
