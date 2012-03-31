903ff9014dc80ce2c1fab5e673d6047c44f13009
Christian Pesch <Christian_Pesch@gmx.de>
2012-03-31 12:58:46 +0200

    remove last route operations from CategoryTreeNodeImpl

---
+++ route-converter/src/main/java/slash/navigation/converter/gui/actions/AddFileAction.java
@@ -0,0 +1,76 @@
+/*
+    This file is part of RouteConverter.
+
+    RouteConverter is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    RouteConverter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RouteConverter; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright (C) 2007 Christian Pesch. All Rights Reserved.
+*/
+
+package slash.navigation.converter.gui.actions;
+
+import slash.navigation.catalog.model.CategoryTreeNode;
+import slash.navigation.converter.gui.RouteConverter;
+import slash.navigation.converter.gui.models.CatalogModel;
+import slash.navigation.gui.FrameAction;
+
+import javax.swing.*;
+import javax.swing.tree.TreePath;
+
+import static java.text.MessageFormat.format;
+import static java.util.Arrays.asList;
+import static javax.swing.JOptionPane.QUESTION_MESSAGE;
+import static javax.swing.JOptionPane.showInputDialog;
+import static slash.common.io.Transfer.trim;
+import static slash.navigation.converter.gui.helper.JTreeHelper.getSelectedCategoryTreeNode;
+import static slash.navigation.converter.gui.helper.JTreeHelper.selectCategoryTreePath;
+
+/**
+ * {@link Action} that adds a category to the {@link CatalogModel}.
+ *
+ * @author Christian Pesch
+ */
+
+public class AddFileAction extends FrameAction {
+    private final JTree tree;
+    private final CatalogModel catalogModel;
+
+    public AddFileAction(JTree tree, CatalogModel catalogModel) {
+        this.tree = tree;
+        this.catalogModel = catalogModel;
+    }
+
+    public void run() {
+        RouteConverter r = RouteConverter.getInstance();
+
+        final CategoryTreeNode category = getSelectedCategoryTreeNode(tree);
+        if (category == null)
+            return;
+
+        final String name = showInputDialog(r.getFrame(),
+                format(RouteConverter.getBundle().getString("add-category-label"), category.getName()),
+                r.getFrame().getTitle(), QUESTION_MESSAGE);
+        if (trim(name) == null)
+            return;
+
+        catalogModel.addCategories(asList(category), asList(name),
+                new Runnable() {
+                    public void run() {
+                        TreePath treePath = new TreePath(catalogModel.getCategoryTreeModel().getPathToRoot(catalogModel.getCategoryTreeModel().getChild(category, name)));
+                        selectCategoryTreePath(tree, treePath);
+                    }
+                });
+
+    }
+}
---
+++ route-converter/src/main/java/slash/navigation/converter/gui/actions/AddUrlAction.java
@@ -0,0 +1,62 @@
+/*
+    This file is part of RouteConverter.
+
+    RouteConverter is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    RouteConverter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RouteConverter; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright (C) 2007 Christian Pesch. All Rights Reserved.
+*/
+
+package slash.navigation.converter.gui.actions;
+
+import slash.navigation.catalog.domain.Category;
+import slash.navigation.converter.gui.RouteConverter;
+import slash.navigation.converter.gui.models.CatalogModel;
+import slash.navigation.gui.FrameAction;
+
+import javax.swing.*;
+import java.io.File;
+
+import static java.util.Arrays.asList;
+import static javax.swing.JFileChooser.APPROVE_OPTION;
+import static javax.swing.JFileChooser.FILES_ONLY;
+import static slash.navigation.gui.Constants.createJFileChooser;
+
+/**
+ * {@link Action} that adds a {@link File} to a {@link Category} to the {@link CatalogModel}.
+ *
+ * @author Christian Pesch
+ */
+
+public class AddUrlAction extends FrameAction {
+    public void run() {
+        RouteConverter r = RouteConverter.getInstance();
+
+        JFileChooser chooser = createJFileChooser();
+        chooser.setDialogTitle(RouteConverter.getBundle().getString("add-file"));
+        chooser.setSelectedFile(r.getUploadRoutePreference());
+        chooser.setFileSelectionMode(FILES_ONLY);
+        chooser.setMultiSelectionEnabled(true);
+        int open = chooser.showOpenDialog(r.getFrame());
+        if (open != APPROVE_OPTION)
+            return;
+
+        File[] selected = chooser.getSelectedFiles();
+        if (selected == null || selected.length == 0)
+            return;
+
+        r.setUploadRoutePreference(selected[0]);
+        r.addFilesToCatalog(asList(selected));
+    }
+}
