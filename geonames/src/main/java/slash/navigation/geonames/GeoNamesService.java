76603849e44e6ad267fe23f63725d987f1169589
Christian Pesch <Christian_Pesch@gmx.de>
2012-04-13 09:14:10 +0200

    interpret limit exceeded

--- geonames/src/main/java/slash/navigation/geonames/GeoNamesService.java
+++ geonames/src/main/java/slash/navigation/geonames/GeoNamesService.java
@@ -79,7 +79,7 @@ public class GeoNamesService {
     }
 
     private void checkCurrentlyOverloaded(String url, String result) throws ServiceUnavailableException {
-        if (result.contains("<html>") && result.contains("currently overloaded"))
+        if (result.contains("<html>") && (result.contains("overloaded") || result.contains("exceeded")))
             throw new ServiceUnavailableException("geonames.org", url);
     }
 
