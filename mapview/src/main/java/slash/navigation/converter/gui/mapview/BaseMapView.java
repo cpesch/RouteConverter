c57ae7aeee39c8f8b0b231283da29e85e7013ea2
Christian Pesch <Christian_Pesch@gmx.de>
2012-05-23 23:26:28 +0200

    use different mapType preference for 2.7

--- mapview/src/main/java/slash/navigation/converter/gui/mapview/BaseMapView.java
+++ mapview/src/main/java/slash/navigation/converter/gui/mapview/BaseMapView.java
@@ -98,7 +98,7 @@ public abstract class BaseMapView implements MapView {
     protected static final Preferences preferences = Preferences.userNodeForPackage(MapView.class);
     protected static final Logger log = Logger.getLogger(MapView.class.getName());
 
-    protected static final String MAP_TYPE_PREFERENCE = "mapType3";
+    protected static final String MAP_TYPE_PREFERENCE = "mapType3+";
     private static final String CLEAN_ELEVATION_ON_MOVE_PREFERENCE = "cleanElevationOnMove";
     private static final String CLEAN_TIME_ON_MOVE_PREFERENCE = "cleanTimeOnMove";
     private static final String COMPLEMENT_TIME_ON_MOVE_PREFERENCE = "complementTimeOnMove";
