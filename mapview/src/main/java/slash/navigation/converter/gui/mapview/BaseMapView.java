cpesch/RouteConverter

commit f18a70f2c71122cc8928c33e5cf929255c616710

reduce number of center-changed callbacks avoid map.getZoom() from Java to JavaScript

mapview/src/main/java/slash/navigation/converter/gui/mapview/BaseMapView.java
@@ -717,6 +717,10 @@ public abstract class BaseMapView implements MapView {
         this.center = center;
     }
 
+    private int getZoom() {
+        return preferences.getInt(CENTER_ZOOM_PREFERENCE, 1);
+    }
+
     protected abstract BaseNavigationPosition getNorthEastBounds();
     protected abstract BaseNavigationPosition getSouthWestBounds();
     protected abstract BaseNavigationPosition getCurrentMapCenter();
@@ -772,7 +776,7 @@ public abstract class BaseMapView implements MapView {
     private List<BaseNavigationPosition> filterSignificantPositions(List<BaseNavigationPosition> positions) {
         long start = currentTimeMillis();
 
-        int zoomLevel = getCurrentZoomLevel();
+        int zoomLevel = getZoom();
         BitSet pointStatus = calculateSignificantPositionsForZoomLevel(positions, zoomLevel);
         List<BaseNavigationPosition> result = new ArrayList<BaseNavigationPosition>();
         for (int i = 0; i < positions.size(); i++)
@@ -832,8 +836,6 @@ public abstract class BaseMapView implements MapView {
 
         return positions;
     }
-
-    protected abstract int getCurrentZoomLevel();
 
     private List<BaseNavigationPosition> filterVisiblePositions(List<BaseNavigationPosition> positions,
                                                                 double factor, boolean includeFirstAndLastPosition) {
@@ -1306,8 +1308,8 @@ public abstract class BaseMapView implements MapView {
     private static final Pattern SELECT_POSITION_PATTERN = Pattern.compile("^select-position/(.*)/(.*)/(.*)/(.*)$");
     private static final Pattern SELECT_POSITIONS_PATTERN = Pattern.compile("^select-positions/(.*)/(.*)/(.*)/(.*)/(.*)");
     private static final Pattern MAP_TYPE_CHANGED_PATTERN = Pattern.compile("^map-type-changed/(.*)$");
-    private static final Pattern ZOOM_CHANGED_PATTERN = Pattern.compile("^zoom-changed$");
-    private static final Pattern CENTER_CHANGED_PATTERN = Pattern.compile("^center-changed/(.*)/(.*)/(.*)$");
+    private static final Pattern ZOOM_CHANGED_PATTERN = Pattern.compile("^zoom-changed/(.*)$");
+    private static final Pattern CENTER_CHANGED_PATTERN = Pattern.compile("^center-changed/(.*)/(.*)$");
     private static final Pattern CALLBACK_PORT_PATTERN = Pattern.compile("^callback-port/(\\d+)$");
     private static final Pattern INSERT_WAYPOINTS_PATTERN = Pattern.compile("^(Insert-All-Waypoints|Insert-Only-Turnpoints): (-?\\d+)/(.*)$");
 
@@ -1411,6 +1413,8 @@ public abstract class BaseMapView implements MapView {
 
         Matcher zoomChangedMatcher = ZOOM_CHANGED_PATTERN.matcher(callback);
         if (zoomChangedMatcher.matches()) {
+            Integer zoom = parseInt(zoomChangedMatcher.group(1));
+            preferences.putInt(CENTER_ZOOM_PREFERENCE, zoom);
             synchronized (notificationMutex) {
                 // since setCenter() leads to a callback and thus paints the track twice
                 if (ignoreNextZoomCallback)
@@ -1421,10 +1425,8 @@ public abstract class BaseMapView implements MapView {
                     if (recenterAfterZooming)
                         haveToRecenterMap = true;
                     haveToRepaintSelectionImmediately = true;
-                    int currentZoomLevel = getCurrentZoomLevel();
-                    preferences.putInt(CENTER_ZOOM_PREFERENCE, currentZoomLevel);
-                    selectionUpdateReason = "zoomed from " + lastZoomLevel + " to " + currentZoomLevel;
-                    lastZoomLevel = currentZoomLevel;
+                    selectionUpdateReason = "zoomed from " + lastZoomLevel + " to " + zoom;
+                    lastZoomLevel = zoom;
                     notificationMutex.notifyAll();
                 }
             }
@@ -1435,7 +1437,6 @@ public abstract class BaseMapView implements MapView {
         if (centerChangedMatcher.matches()) {
             preferences.putDouble(CENTER_LATITUDE_PREFERENCE, parseDouble(centerChangedMatcher.group(1)));
             preferences.putDouble(CENTER_LONGITUDE_PREFERENCE, parseDouble(centerChangedMatcher.group(2)));
-            preferences.putInt(CENTER_ZOOM_PREFERENCE, parseInt(centerChangedMatcher.group(3)));
             if (visibleNorthEast != null && visibleSouthWest != null && visibleNorthWest != null && visibleSouthEast != null) {
                 BaseNavigationPosition mapNorthEast = getNorthEastBounds();
                 BaseNavigationPosition mapSouthWest = getSouthWestBounds();

mapview/src/main/resources/slash/navigation/converter/gui/mapview/routeconverter.html
@@ -626,11 +626,18 @@
            }
 
            google.maps.event.addListener(map, "zoom_changed", function() {
-               callJava("zoom-changed");
+               var zoom = map.getZoom();
+               callJava("zoom-changed/" + zoom);
            });
+           var centerLat = 0.0, centerLng = 0.0;
            google.maps.event.addListener(map, "center_changed", function() {
-               center = map.getCenter();
-               callJava("center-changed/" + center.lat() + "/" + center.lng() + "/" + map.getZoom());
+               var center = map.getCenter();
+               var currentLat = Math.round(center.lat() * 100000) / 100000;
+               var currentLng = Math.round(center.lng() * 100000) / 100000;
+               if(currentLat != centerLat && currentLng != centerLng) {
+                   centerLat = currentLat; centerLng = currentLng;
+                   callJava("center-changed/" + centerLat + "/" + centerLng);
+               }
            });
 
            var shiftKey = false, altKey = false, ctrlKey = false;

swt-mapview/src/main/java/slash/navigation/converter/gui/mapview/EclipseSWTMapView.java
@@ -28,8 +28,8 @@ import chrriis.dj.nativeswing.swtimpl.components.WebBrowserNavigationEvent;
 import chrriis.dj.nativeswing.swtimpl.components.WebBrowserWindowOpeningEvent;
 import chrriis.dj.nativeswing.swtimpl.components.WebBrowserWindowWillOpenEvent;
 import slash.common.io.Externalization;
-import slash.common.system.Platform;
 import slash.common.io.TokenResolver;
+import slash.common.system.Platform;
 import slash.navigation.base.BaseNavigationPosition;
 
 import javax.swing.*;
@@ -43,7 +43,6 @@ import static chrriis.dj.nativeswing.swtimpl.NativeInterface.isOpen;
 import static chrriis.dj.nativeswing.swtimpl.components.JWebBrowser.useWebkitRuntime;
 import static chrriis.dj.nativeswing.swtimpl.components.JWebBrowser.useXULRunnerRuntime;
 import static slash.common.system.Platform.isLinux;
-import static slash.common.io.Transfer.parseDouble;
 
 /**
  * Implementation for a component that displays the positions of a position list on a map.
@@ -283,12 +282,7 @@ public class EclipseSWTMapView extends BaseMapView {
         }
     }
 
-    // zoom level and bounds
-
-    protected int getCurrentZoomLevel() {
-        String zoomLevel = executeScriptWithResult("return map.getZoom();");
-        return zoomLevel != null ? parseDouble(zoomLevel).intValue() : 1;
-    }
+    // bounds and center
 
     protected BaseNavigationPosition getNorthEastBounds() {
         return extractLatLng("return getNorthEastBounds();");

commit 617d46097439f958d5dddaf8526af9f507b5bd76

fix zoom problems introduced with f18a70f2c71122cc8928c33e5cf929255c616710

mapview/src/main/java/slash/navigation/converter/gui/mapview/BaseMapView.java
@@ -725,9 +725,14 @@ public abstract class BaseMapView implements MapView {
         return preferences.getInt(CENTER_ZOOM_PREFERENCE, 1);
     }
 
+    private void setZoom(int zoom) {
+        preferences.putInt(CENTER_ZOOM_PREFERENCE, zoom);
+    }
+
     protected abstract BaseNavigationPosition getNorthEastBounds();
     protected abstract BaseNavigationPosition getSouthWestBounds();
     protected abstract BaseNavigationPosition getCurrentMapCenter();
+    protected abstract Integer getCurrentZoom();
 
     private BaseNavigationPosition getLastMapCenter() {
         double latitude = preferences.getDouble(CENTER_LATITUDE_PREFERENCE, 35.0);
@@ -914,7 +919,7 @@ public abstract class BaseMapView implements MapView {
 
         long end = currentTimeMillis();
         log.info(format("Filtered selected positions to reduce %d positions to %d in %d milliseconds",
-                positions.size(), result.size(), (end - start)));
+                selectedIndices.length, result.size(), (end - start)));
         return result;
     }
 
@@ -1084,7 +1089,8 @@ public abstract class BaseMapView implements MapView {
     private void setCenterOfMap(List<BaseNavigationPosition> positions, boolean recenter) {
         StringBuilder buffer = new StringBuilder();
 
-        if (positions.size() > 0 && recenter) {
+        boolean fitBoundsToPositions = positions.size() > 0 && recenter;
+        if (fitBoundsToPositions) {
             BaseNavigationPosition northEast = northEast(positions);
             BaseNavigationPosition southWest = southWest(positions);
             buffer.append("fitBounds(").append(southWest.getLatitude()).append(",").append(southWest.getLongitude()).append(",").
@@ -1106,6 +1112,11 @@ public abstract class BaseMapView implements MapView {
         }
         executeScript(buffer.toString());
         haveToInitializeMapOnFirstStart = false;
+
+        if (fitBoundsToPositions) {
+            // need to update zoom since fitBounds() changes the zoom level without firing a notification
+            setZoom(getCurrentZoom());
+        }
     }
 
     private void selectPositions(List<BaseNavigationPosition> selectedPositions, BaseNavigationPosition center) {
@@ -1418,7 +1429,8 @@ public abstract class BaseMapView implements MapView {
         Matcher zoomChangedMatcher = ZOOM_CHANGED_PATTERN.matcher(callback);
         if (zoomChangedMatcher.matches()) {
             Integer zoom = parseInt(zoomChangedMatcher.group(1));
-            preferences.putInt(CENTER_ZOOM_PREFERENCE, zoom);
+            setZoom(zoom);
+
             synchronized (notificationMutex) {
                 // since setCenter() leads to a callback and thus paints the track twice
                 if (ignoreNextZoomCallback)

mapview/src/main/resources/slash/navigation/converter/gui/mapview/routeconverter.html
@@ -393,6 +393,10 @@
        return map.getCenter().toUrlValue(6);
    }
 
+   function getZoom() {
+       return map.getZoom();
+   }
+
    function getThresholdForPixel(map, latLng, pixel) {
        var worldCoordinate = map.getProjection().fromLatLngToPoint(latLng);
        var scale = Math.pow(2, map.getZoom());

swt-mapview/src/main/java/slash/navigation/converter/gui/mapview/EclipseSWTMapView.java
@@ -29,7 +29,6 @@ import chrriis.dj.nativeswing.swtimpl.components.WebBrowserWindowOpeningEvent;
 import chrriis.dj.nativeswing.swtimpl.components.WebBrowserWindowWillOpenEvent;
 import slash.common.io.Externalization;
 import slash.common.io.TokenResolver;
-import slash.common.system.Platform;
 import slash.navigation.base.BaseNavigationPosition;
 
 import javax.swing.*;
@@ -42,6 +41,8 @@ import java.util.logging.Logger;
 import static chrriis.dj.nativeswing.swtimpl.NativeInterface.isOpen;
 import static chrriis.dj.nativeswing.swtimpl.components.JWebBrowser.useWebkitRuntime;
 import static chrriis.dj.nativeswing.swtimpl.components.JWebBrowser.useXULRunnerRuntime;
+import static slash.common.io.Transfer.parseDouble;
+import static slash.common.io.Transfer.parseInt;
 import static slash.common.system.Platform.isLinux;
 import static slash.common.system.Platform.isMac;
 import static slash.common.system.Platform.isWindows;
@@ -296,6 +297,11 @@ public class EclipseSWTMapView extends BaseMapView {
 
     protected BaseNavigationPosition getCurrentMapCenter() {
         return extractLatLng("return getCenter();");
+    }
+
+    protected Integer getCurrentZoom() {
+        Double zoom = parseDouble(executeScriptWithResult("return getZoom();"));
+        return zoom != null ? zoom.intValue() : null;
     }
 
     // script execution