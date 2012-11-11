b474c6f645ae6a50e27320fa43fee27e9c766a09
Christian Pesch <Christian_Pesch@gmx.de>
2012-11-11 17:30:11 +0100

    trying to solve
    java.lang.NoSuchMethodError: slash.navigation.bcr.BcrPosition.calculateDistance(Lslash/navigation/base/BaseNavigationPosition;)Ljava/lang/Double;

--- navigation-formats/src/main/java/slash/navigation/base/NavigationPosition.java
+++ navigation-formats/src/main/java/slash/navigation/base/NavigationPosition.java
@@ -88,7 +88,7 @@ public interface NavigationPosition {
      * @return the distance in meters between this and the other position
      *         or null if the distance cannot be calculated
      */
-    Double calculateDistance(NavigationPosition other);
+    <P extends NavigationPosition> Double calculateDistance(P other);
     Double calculateDistance(double longitude, double latitude);
 
     /**
