4cec50e59412ba22a5bdf5c9de191e85ef06465c
Christian Pesch <Christian_Pesch@gmx.de>
2013-05-02 17:21:25 +0200

    allows to interpolate for negative values

--- hgt/src/main/java/slash/navigation/hgt/ElevationTile.java
+++ hgt/src/main/java/slash/navigation/hgt/ElevationTile.java
@@ -30,6 +30,9 @@ import java.io.RandomAccessFile;
  */
 
 public class ElevationTile {
+    private static final int NUMBER_OF_INTERVALS = 1200; // 1200 Intervals means 1201 positions per line and column
+    private static final int INVALID_VALUE_LIMIT = -15000; // Won't interpolate below this elevation in Meters, guess is: -0x8000
+
     private RandomAccessFile elevationFile;
 
     public ElevationTile(RandomAccessFile elevationFile) {
@@ -41,7 +44,7 @@ public class ElevationTile {
      * theorem on intersecting lines (Strahlensatz).
      *
      * @param dHeight12 the delta height/elevation of two sub tile positions
-     * @param dLength12 the length of an sub tile interval (1 / 1200)
+     * @param dLength12 the length of an sub tile interval (1 / NUMBER_OF_INTERVALS)
      * @param dDiff     the distance of the real point from the sub tile position
      * @return the delta elevation (relative to sub tile position)
      */
@@ -50,39 +53,36 @@ public class ElevationTile {
     }
 
     public Double getElevationFor(Double longitude, Double latitude) throws IOException {
-        if (elevationFile == null)
+        if (elevationFile == null || longitude == null || latitude == null)
             return null;
 
-        double dElevation;
-        double dLon = longitude;
-        double dLat = latitude;
-        int nLon = (int) dLon;                    // Cut off the decimal places
-        int nLat = (int) dLat;                    // Cut off the decimal places
-        int nAS = 1200;                           // 1200 Intervals (means 1201 positions per line and column)
+        // cut off the decimal places
+        int longitudeAsInt = longitude.intValue();
+        int latitudeAsInt = latitude.intValue();
 
-        if (dLon < 0) {                                        // If it's west longitude (negative value)
-            nLon = (nLon - 1) * -1;                            // Make a positive number (left edge)
-            dLon = ((double) nLon + dLon) + (double) nLon;     // Make positive double longitude (needed for later calculation)
+        if (longitude < 0) {                                        // If it's west longitude (negative value)
+            longitudeAsInt = (longitudeAsInt - 1) * -1;             // Make a positive number (left edge)
+            longitude = ((double) longitudeAsInt + longitude) + (double) longitudeAsInt; // Make positive double longitude (needed for later calculation)
         }
 
-        if (dLat < 0) {                                        // If it's a south latitude (negative value)
-            nLat = (nLat - 1) * -1;                            // Make a positive number (bottom edge)
-            dLat = ((double) nLat + dLat) + (double) nLat;     // Make positive double latitude (needed for later calculation)
+        if (latitude < 0) {                                        // If it's a south latitude (negative value)
+            latitudeAsInt = (latitudeAsInt - 1) * -1;              // Make a positive number (bottom edge)
+            latitude = ((double) latitudeAsInt + latitude) + (double) latitudeAsInt; // Make positive double latitude (needed for later calculation)
         }
 
-        int nLonIndex = (int) ((dLon - (double) nLon) * nAS);  // Calculate the interval index for longitude
-        int nLatIndex = (int) ((dLat - (double) nLat) * nAS);  // Calculate the interval index for latitude
+        int nLonIndex = (int) ((longitude - (double) longitudeAsInt) * NUMBER_OF_INTERVALS); // Calculate the interval index for longitude
+        int nLatIndex = (int) ((latitude - (double) latitudeAsInt) * NUMBER_OF_INTERVALS);   // Calculate the interval index for latitude
 
-        if (nLonIndex >= nAS) {
-            nLonIndex = nAS - 1;
+        if (nLonIndex >= NUMBER_OF_INTERVALS) {
+            nLonIndex = NUMBER_OF_INTERVALS - 1;
         }
 
-        if (nLatIndex >= nAS) {
-            nLatIndex = nAS - 1;
+        if (nLatIndex >= NUMBER_OF_INTERVALS) {
+            nLatIndex = NUMBER_OF_INTERVALS - 1;
         }
 
-        double dOffLon = dLon - (double) nLon;                      // The lon value offset within a tile
-        double dOffLat = dLat - (double) nLat;                      // The lat value offset within a tile
+        double dOffLon = longitude - (double) longitudeAsInt;                    // The longitude value offset within a tile
+        double dOffLat = latitude - (double) latitudeAsInt;                      // The latitude value offset within a tile
 
         double dLeftTop;                                            // The left top position of a sub tile
         double dLeftBottom;                                         // The left bottom position of a sub tile
@@ -90,44 +90,41 @@ public class ElevationTile {
         double dRightBottom;                                        // The right bootm position of a sub tile
         int pos;                                                    // The index of the elevation into the hgt file
 
-        pos = (((nAS - nLatIndex) - 1) * (nAS + 1)) + nLonIndex;    // The index for the left top elevation
+        pos = (((NUMBER_OF_INTERVALS - nLatIndex) - 1) * (NUMBER_OF_INTERVALS + 1)) + nLonIndex; // The index for the left top elevation
         elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
         dLeftTop = elevationFile.readShort();                       // Now read the left top elevation from hgt file
 
-        pos = ((nAS - nLatIndex) * (nAS + 1)) + nLonIndex;          // The index for the left bottom elevation
+        pos = ((NUMBER_OF_INTERVALS - nLatIndex) * (NUMBER_OF_INTERVALS + 1)) + nLonIndex; // The index for the left bottom elevation
         elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
         dLeftBottom = elevationFile.readShort();                    // Now read the left bottom elevation from hgt file
 
-        pos = (((nAS - nLatIndex) - 1) * (nAS + 1)) + nLonIndex + 1;// The index for the right top elevation
+        pos = (((NUMBER_OF_INTERVALS - nLatIndex) - 1) * (NUMBER_OF_INTERVALS + 1)) + nLonIndex + 1; // The index for the right top elevation
         elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
         dRightTop = elevationFile.readShort();                      // Now read the right top elevation from hgt file
 
-        pos = ((nAS - nLatIndex) * (nAS + 1)) + nLonIndex + 1;      // The index for the right bottom elevation
+        pos = ((NUMBER_OF_INTERVALS - nLatIndex) * (NUMBER_OF_INTERVALS + 1)) + nLonIndex + 1; // The index for the right bottom elevation
         elevationFile.seek(pos * 2);                                // We have 16-bit values for elevation, so multiply by 2
         dRightBottom = elevationFile.readShort();                   // Now read the right bottom top elevation from hgt file
 
-        if ((dLeftTop < 0) ||                                       // If one of the elevation values
-                (dLeftBottom < 0) ||                                // we read from
-                (dRightTop < 0) ||                                  // the hgt file is
-                (dRightBottom < 0)) {                               // not valid
-            return null;                                            // we can't interpolate
+        // if one of the read elevation values is not valid, we cannot interpolate
+        if ((dLeftTop < INVALID_VALUE_LIMIT) || (dLeftBottom < INVALID_VALUE_LIMIT) ||
+                (dRightTop < INVALID_VALUE_LIMIT) || (dRightBottom < INVALID_VALUE_LIMIT)) {
+            return null;
         }
 
-        double dDeltaLat;         // The delta between top lat value and wanted lat (delta within a sub tile)
-        double dDeltaLon;         // The delta between left lon value and wanted lon (delta within a sub tile)
+        // the delta between top lat value and requested latitude (offset within a sub tile)
+        double dDeltaLon = dOffLon - (double) nLonIndex * (1.0 / (double) NUMBER_OF_INTERVALS);
+        // the delta between left lon value and requested longitude (offset within a sub tile)
+        double dDeltaLat = dOffLat - (double) nLatIndex * (1.0 / (double) NUMBER_OF_INTERVALS);
 
-        dDeltaLon = dOffLon - (double) nLonIndex * (1.0 / (double) nAS);   // The delta (offset) from left point to wanted point
-        dDeltaLat = dOffLat - (double) nLatIndex * (1.0 / (double) nAS);   // The delta (offset) from bottom point to wanted point
+        // the interpolated elevation calculated from left top to left bottom
+        double dLonHeightLeft = dLeftBottom - calculateElevation(dLeftBottom - dLeftTop, 1.0 / (double) NUMBER_OF_INTERVALS, dDeltaLat);
+        // the interpolated elevation calculated from right top to right bottom
+        double dLonHeightRight = dRightBottom - calculateElevation(dRightBottom - dRightTop, 1.0 / (double) NUMBER_OF_INTERVALS, dDeltaLat);
 
-        double dLonHeightLeft;    // The interpolated elevation calculated from left top to left bottom
-        double dLonHeightRight;   // The interpolated elevation calculated from right top to right bottom
-
-        dLonHeightLeft = dLeftBottom - calculateElevation(dLeftBottom - dLeftTop, 1.0 / (double) nAS, dDeltaLat);
-        dLonHeightRight = dRightBottom - calculateElevation(dRightBottom - dRightTop, 1.0 / (double) nAS, dDeltaLat);
-
-        // Interpolate between the interpolated left elevation and interpolated right elevation
-        dElevation = dLonHeightLeft - calculateElevation(dLonHeightLeft - dLonHeightRight, 1.0 / (double) nAS, dDeltaLon);
-
-        return dElevation + 0.5;   // Do a rounding of the calculated elevation
+        // interpolate between the interpolated left elevation and interpolated right elevation
+        double dElevation = dLonHeightLeft - calculateElevation(dLonHeightLeft - dLonHeightRight, 1.0 / (double) NUMBER_OF_INTERVALS, dDeltaLon);
+        // round the interpolated elevation
+        return dElevation + 0.5;
     }
 }
