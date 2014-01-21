48d18abfc025482a7827605ffacfa5eea1f94df6
Christian Pesch <Christian_Pesch@gmx.de>
2014-01-21 09:03:52 +0100

    rename module hgt-tools to download-tools

--- hgt-tools/src/main/doc/create-hgt-datasources-xml.bat
+++
@@ -1,6 +0,0 @@
-java -jar hgt-tools\target\CreateHgtDataSourcesXml.jar "test" "testUrl" "testDirectory" "C:\Temp\test-hgt" "test-datasources.xml"
-java -jar hgt-tools\target\CreateHgtDataSourcesXml.jar "NASA SRTM 3" "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/" "srtm3" "W:\Mirrors\dds.cr.usgs.gov\srtm\version2_1\SRTM3" "hgt\src\main\resources\slash\navigation\hgt\srtm3-datasources.xml"
-java -jar hgt-tools\target\CreateHgtDataSourcesXml.jar "NASA SRTM 1" "http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/" "srtm1" "W:\Mirrors\dds.cr.usgs.gov\srtm\version2_1\SRTM1" "hgt\src\main\resources\slash\navigation\hgt\srtm1-datasources.xml"
-java -jar hgt-tools\target\CreateHgtDataSourcesXml.jar "Jonathan de Ferranti DEM 3" "http://www.viewfinderpanoramas.org/dem3/" "ferranti3" "W:\Mirrors\www.viewfinderpanoramas.org\dem3" "hgt\src\main\resources\slash\navigation\hgt\ferranti3-datasources.xml"
-java -jar hgt-tools\target\CreateHgtDataSourcesXml.jar "Jonathan de Ferranti DEM 1" "http://www.viewfinderpanoramas.org/dem1/" "ferranti1" "W:\Mirrors\www.viewfinderpanoramas.org\dem1" "hgt\src\main\resources\slash\navigation\hgt\ferranti1-datasources.xml"
-
