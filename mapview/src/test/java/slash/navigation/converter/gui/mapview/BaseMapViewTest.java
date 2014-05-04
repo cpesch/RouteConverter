8c329c8ca16aa8e9db683789a3e655e68394ca40
lundefugl <montgomery_scott@gmx.de>
2014-05-04 13:40:06 +0200

    Merge git://github.com/cpesch/RouteConverter

--- mapview/src/test/java/slash/navigation/converter/gui/mapview/BaseMapViewTest.java
+++ mapview/src/test/java/slash/navigation/converter/gui/mapview/BaseMapViewTest.java
@@ -20,15 +20,10 @@
 
 package slash.navigation.converter.gui.mapview;
 
-import org.junit.Before;
 import org.junit.Test;
-import slash.navigation.base.BaseNavigationPosition;
 
-import javax.management.RuntimeErrorException;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
@@ -38,83 +33,6 @@ import static slash.common.io.Transfer.ceiling;
 
 public class BaseMapViewTest {
     private static final int MAXIMUM_DIRECTIONS_SEGMENT_LENGTH = 4;
-    private BaseMapView view;
-
-    @Before
-    public void setUp() throws Exception {
-        view = new BaseMapView() {
-            @Override
-            protected void initializeBrowser() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            protected BaseNavigationPosition getNorthEastBounds() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            protected BaseNavigationPosition getSouthWestBounds() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            protected BaseNavigationPosition getCurrentMapCenter() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            protected Integer getCurrentZoom() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            protected void executeScript(String script) {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            protected String executeScriptWithResult(String script) {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            public boolean isSupportedPlatform() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            public Component getComponent() {
-                throw new RuntimeException("not implemented");
-            }
-
-            @Override
-            public void resize() {
-                throw new RuntimeException("not implemented");
-            }
-        };
-    }
-
-    @Test
-    public void testFilterEveryNthPosition() throws Exception {
-
-        final Random random = new Random();
-
-        final int outCount = random.nextInt(200) + 2;
-        final int inCount = random.nextInt(500000) + outCount + 1;
-
-        final ArrayList<Integer> inList = new ArrayList<Integer>();
-        for (int i=0; i<inCount; i++) {
-            inList.add(random.nextInt());
-        }
-        assertEquals(inList.size(), inCount);
-
-        List<Integer> result = view.filterEveryNthPosition(inList, outCount);
-
-        assertEquals(outCount, result.size());
-        assertEquals(inList.get(0), result.get(0));
-        assertEquals(inList.get(inList.size()-1), result.get(result.size()-1));
-    }
 
     private List<Integer> createIntervals(int size) {
         List<Integer> result = new ArrayList<Integer>();
