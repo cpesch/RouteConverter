e40dc7c97901b76b3315939151cf517d03fdd3e5
Fredrik Teschke <f@ftes.de>
2014-06-16 00:23:01 +0200

    remove NamespacePrefixMapperImpl in favor of specifying NS in package-info.java - the interface NamespacePrefixMapper lies in an internal package - depending on the compiler settings, this leads to a compilation error (if access to rt.jar-internal packages is disallowed) - see https://issues.apache.org/jira/browse/SIS-74 for a related discussion on exchanging the NamespacePrefixMapper-based approach

--- kml/src/main/java/slash/navigation/kml/binding20/package-info.java
+++ kml/src/main/java/slash/navigation/kml/binding20/package-info.java
@@ -1,5 +1,12 @@
 /**
  * Contains the <a href="https://jaxb.dev.java.net/nonav/2.0.5/docs/api/">JAXB</a> binding for the Kml20Format.
  */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://earth.google.com/kml/2.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = KmlUtil.KML_20_NAMESPACE_URI, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+		@XmlNs(prefix = "", namespaceURI = KmlUtil.KML_20_NAMESPACE_URI),
+		@XmlNs(prefix = "xal", namespaceURI = KmlUtil.XAL_20_NAMESPACE_URI) })
 package slash.navigation.kml.binding20;
+
+import javax.xml.bind.annotation.XmlNs;
+
+import slash.navigation.kml.KmlUtil;
+
--- kml/src/main/java/slash/navigation/kml/binding22/package-info.java
+++ kml/src/main/java/slash/navigation/kml/binding22/package-info.java
@@ -1,5 +1,14 @@
 /**
  * Contains the <a href="https://jaxb.dev.java.net/nonav/2.0.5/docs/api/">JAXB</a> binding for the Kml22Format.
  */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/kml/2.2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = KmlUtil.KML_22_NAMESPACE_URI, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+		@XmlNs(prefix = "", namespaceURI = KmlUtil.KML_22_NAMESPACE_URI),
+		@XmlNs(prefix = "atom", namespaceURI = KmlUtil.ATOM_2005_NAMESPACE_URI),
+		@XmlNs(prefix = "xal", namespaceURI = KmlUtil.XAL_20_NAMESPACE_URI),
+		@XmlNs(prefix = "gx", namespaceURI = KmlUtil.KML_22_EXT_NAMESPACE_URI) })
 package slash.navigation.kml.binding22;
+
+import javax.xml.bind.annotation.XmlNs;
+
+import slash.navigation.kml.KmlUtil;
+
