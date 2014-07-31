1fc078a91af1da06a1051e27308d04c201868a70
Christian Pesch <Christian_Pesch@gmx.de>
2014-07-31 21:06:10 +0200

    do not try to download from online elevation services

--- route-converter/src/main/java/slash/navigation/converter/gui/helpers/BatchPositionAugmenter.java
+++ route-converter/src/main/java/slash/navigation/converter/gui/helpers/BatchPositionAugmenter.java
@@ -120,9 +120,13 @@ public class BatchPositionAugmenter {
 
     private interface Operation {
         String getName();
+
         int getColumnIndex();
+
         void performOnStart();
+
         boolean run(int index, NavigationPosition position) throws Exception;
+
         String getErrorMessage();
     }
 
@@ -290,6 +294,8 @@ public class BatchPositionAugmenter {
     }
 
     private void downloadElevationData(int[] rows) {
+        if (!elevationServiceFacade.isDownload())
+            return;
         List<LongitudeAndLatitude> longitudeAndLatitudes = new ArrayList<LongitudeAndLatitude>();
         for (int row : rows) {
             NavigationPosition position = positionsModel.getPosition(row);
