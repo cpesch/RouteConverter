68164f3d5ed68721e8b394ab69f04f6e9687c3bb
Christian Pesch <Christian_Pesch@gmx.de>
2015-02-07 13:02:29 +0100

    do not test presence of mac keystrokes

--- route-converter/src/test/java/slash/navigation/converter/gui/ResourceBundleTest.java
+++ route-converter/src/test/java/slash/navigation/converter/gui/ResourceBundleTest.java
@@ -54,7 +54,7 @@ public class ResourceBundleTest {
             String key = keys.nextElement();
             // skip keys which are only present in the default bundle
             if (key.startsWith("locale-") || key.endsWith("-icon") ||
-                    key.endsWith("-mnemonic") || key.endsWith("-keystroke") ||
+                    key.endsWith("-mnemonic") || key.endsWith("-keystroke") || key.endsWith("-keystroke-mac") ||
                     key.equals("help-set") || key.equals("translator-missing") ||
                     key.equals("FileChooser.acceptAllFileFilterText"))
                 continue;
