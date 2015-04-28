ad94f8f74584ded25d3c090e383e3018717a5824
Christian Pesch <Christian_Pesch@gmx.de>
2015-04-28 15:30:23 +0200

    initialize routing services at the very first occasion

--- route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter.java
+++ route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter.java
@@ -328,6 +328,8 @@ public class RouteConverter extends SingleFrameApplication {
 
         invokeLater(new Runnable() {
             public void run() {
+                initializeRoutingServices();
+
                 getMapView().initialize(getPositionsModel(),
                         getPositionsSelectionModel(),
                         getConvertPanel().getCharacteristicsModel(),
