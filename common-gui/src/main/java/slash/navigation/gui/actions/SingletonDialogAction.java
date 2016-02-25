b1675851600ebc7267627318f429ec46848fff42
Christian Pesch <Christian_Pesch@gmx.de>
2016-02-25 09:27:10 +0100

    better error message when opening a dialog fails

--- common-gui/src/main/java/slash/navigation/gui/actions/SingletonDialogAction.java
+++ common-gui/src/main/java/slash/navigation/gui/actions/SingletonDialogAction.java
@@ -22,6 +22,12 @@ package slash.navigation.gui.actions;
 
 import slash.navigation.gui.SimpleDialog;
 
+import java.util.logging.Logger;
+
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
+import static slash.common.helpers.ExceptionHelper.getLocalizedMessage;
+
 /**
  * Show a dialog at most once.
  *
@@ -29,19 +35,26 @@ import slash.navigation.gui.SimpleDialog;
  */
 
 public abstract class SingletonDialogAction extends FrameAction {
+    private static final Logger log = Logger.getLogger(SingletonDialogAction.class.getName());
     private SimpleDialog dialog = null;
 
     protected abstract SimpleDialog createDialog();
 
     public void run() {
-        if (dialog == null) {
-            dialog = createDialog();
-            dialog.pack();
-            dialog.restoreLocation();
-        }
-        if (!dialog.isVisible()) {
-            dialog.toFront();
-            dialog.setVisible(true);
+        try {
+            if (dialog == null) {
+                dialog = createDialog();
+                dialog.pack();
+                dialog.restoreLocation();
+            }
+            if (!dialog.isVisible()) {
+                dialog.toFront();
+                dialog.setVisible(true);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            log.severe("Could not open dialog: " + e);
+            showMessageDialog(null, "Could not open dialog: " + getLocalizedMessage(e), "Error", ERROR_MESSAGE);
         }
     }
 }
