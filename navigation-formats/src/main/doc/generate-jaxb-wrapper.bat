468d734edc156842866feadf233690b0454990b9
Christian Pesch <Christian_Pesch@gmx.de>
2016-07-02 09:02:44 +0200

    prepare tileserver xml for Wanderreitkarte

--- navigation-formats/src/main/doc/generate-jaxb-wrapper.bat
+++ navigation-formats/src/main/doc/generate-jaxb-wrapper.bat
@@ -1,30 +1,30 @@
-bin\xjc -verbose -d generated -p slash.navigation.gpx.binding10 c:\p4\RouteConverter\gpx\src\main\doc\gpx10.xsd
-bin\xjc -verbose -d generated -p slash.navigation.gpx.binding11 c:\p4\RouteConverter\gpx\src\main\doc\gpx11.xsd
-bin\xjc -verbose -d generated -p slash.navigation.gpx.garmin3 c:\p4\RouteConverter\gpx\src\main\doc\GpxExtensionsv3.xsd
-bin\xjc -cp lib/jaxb-impl.jar -verbose -d c:\p4\RouteConverter\gpx\src\main\java -p slash.navigation.gpx.trackpoint1 c:\p4\RouteConverter\gpx\src\main\doc\TrackPointExtensionv1.xsd
-bin\xjc -cp lib/jaxb-impl.jar -verbose -d c:\p4\RouteConverter\gpx\src\main\java -p slash.navigation.gpx.trackpoint2 c:\p4\RouteConverter\gpx\src\main\doc\TrackPointExtensionv2.xsd
-bin\xjc -cp lib/jaxb-impl.jar -verbose -d c:\p4\RouteConverter\gpx\src\main\java -p slash.navigation.gpx.trip1 c:\p4\RouteConverter\gpx\src\main\doc\TripExtensionsv1.xsd
-bin\xjc -verbose -d generated -p slash.navigation.gpx.routecatalog10 c:\p4\RouteConverter\gpx\src\main\doc\RouteCatalog10.xsd
-bin\xjc -verbose -d generated -p slash.navigation.gpx.trekbuddy c:\p4\RouteConverter\gpx\src\main\doc\trekBuddyExtensions0984.xsd
-bin\xjc -verbose -d generated -p slash.navigation.lmx.binding c:\p4\RouteConverter\navigation-formats\src\main\doc\lmx\lmx.xsd
-bin\xjc -verbose -d generated -p slash.navigation.tcx.binding1 c:\p4\RouteConverter\navigation-formats\src\main\doc\tcx\TrainingCenterDatabasev1.xsd
-bin\xjc -verbose -d generated -p slash.navigation.tcx.binding2 c:\p4\RouteConverter\navigation-formats\src\main\doc\tcx\TrainingCenterDatabasev2.xsd
-bin\xjc -verbose -d generated -p slash.navigation.kml.binding20 c:\p4\RouteConverter\kml\src\main\doc\kml20.xsd
-bin\xjc -verbose -d generated -p slash.navigation.kml.binding21 c:\p4\RouteConverter\kml\src\main\doc\kml21.xsd
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\kml\src\main\java c:\p4\RouteConverter\kml\src\main\doc\ogckml22.xsd -b c:\p4\RouteConverter\kml\src\main\doc\ogckml22.xjb
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\kml\src\main\java c:\p4\RouteConverter\kml\src\main\doc\kml22gx.xsd -b c:\p4\RouteConverter\kml\src\main\doc\kml22gx.xjb
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\kml\src\main\java c:\p4\RouteConverter\kml\src\main\doc\kml22beta.xsd -b c:\p4\RouteConverter\kml\src\main\doc\kml22beta.xjb
-bin\xjc -verbose -d generated -p slash.navigation.gopal.binding3 c:\p4\RouteConverter\navigation-formats\src\main\doc\gopal\gopal3.xsd
-bin\xjc -verbose -d generated -p slash.navigation.gopal.binding5 c:\p4\RouteConverter\navigation-formats\src\main\doc\gopal\gopal5.xsd
-bin\xjc -verbose -dtd -d generated -p slash.navigation.viamichelin.binding c:\p4\RouteConverter\navigation-formats\src\main\doc\viamichelin\export.dtd
-bin\xjc -verbose -d generated -p slash.navigation.geonames.binding c:\p4\RouteConverter-git\geonames\src\main\doc\geonames.xsd
-bin\xjc -verbose -d generated -p slash.navigation.nmn.binding7 c:\p4\RouteConverter\navigation-formats\src\main\doc\nmn\nmn7.xsd
-bin\xjc -verbose -d generated -p slash.navigation.klicktel.binding c:\p4\RouteConverter\navigation-formats\src\main\doc\klicktel\klicktel.xsd
-bin\xjc -verbose -d generated -p slash.navigation.googlemaps.elevation c:\p4\RouteConverter\googlemaps\src\main\doc\elevation.xsd
-bin\xjc -verbose -d generated -p slash.navigation.googlemaps.geocode c:\p4\RouteConverter\googlemaps\src\main\doc\geocode.xsd
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\datasource\src\main\java -p slash.navigation.datasources.binding c:\p4\RouteConverter\datasource\src\main\doc\datasource-catalog.xsd
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\download\src\main\java -p slash.navigation.download.queue.binding  c:\p4\RouteConverter\download\src\main\doc\queue.xsd
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\route\src\main\java -p slash.navigation.routes.remote.binding c:\p4\RouteConverter\route\src\main\doc\route-catalog.xsd
-bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\p4\RouteConverter\browser-mapview\src\main\java -p slash.navigation.mapview.tileserver.binding c:\p4\RouteConverter\browser-mapview\src\main\doc\tileserver-catalog.xsd
+bin\xjc -verbose -d generated -p slash.navigation.gpx.binding10 c:\Development\RouteConverter\gpx\src\main\doc\gpx10.xsd
+bin\xjc -verbose -d generated -p slash.navigation.gpx.binding11 c:\Development\RouteConverter\gpx\src\main\doc\gpx11.xsd
+bin\xjc -verbose -d generated -p slash.navigation.gpx.garmin3 c:\Development\RouteConverter\gpx\src\main\doc\GpxExtensionsv3.xsd
+bin\xjc -cp lib/jaxb-impl.jar -verbose -d c:\Development\RouteConverter\gpx\src\main\java -p slash.navigation.gpx.trackpoint1 c:\Development\RouteConverter\gpx\src\main\doc\TrackPointExtensionv1.xsd
+bin\xjc -cp lib/jaxb-impl.jar -verbose -d c:\Development\RouteConverter\gpx\src\main\java -p slash.navigation.gpx.trackpoint2 c:\Development\RouteConverter\gpx\src\main\doc\TrackPointExtensionv2.xsd
+bin\xjc -cp lib/jaxb-impl.jar -verbose -d c:\Development\RouteConverter\gpx\src\main\java -p slash.navigation.gpx.trip1 c:\Development\RouteConverter\gpx\src\main\doc\TripExtensionsv1.xsd
+bin\xjc -verbose -d generated -p slash.navigation.gpx.routecatalog10 c:\Development\RouteConverter\gpx\src\main\doc\RouteCatalog10.xsd
+bin\xjc -verbose -d generated -p slash.navigation.gpx.trekbuddy c:\Development\RouteConverter\gpx\src\main\doc\trekBuddyExtensions0984.xsd
+bin\xjc -verbose -d generated -p slash.navigation.lmx.binding c:\Development\RouteConverter\navigation-formats\src\main\doc\lmx\lmx.xsd
+bin\xjc -verbose -d generated -p slash.navigation.tcx.binding1 c:\Development\RouteConverter\navigation-formats\src\main\doc\tcx\TrainingCenterDatabasev1.xsd
+bin\xjc -verbose -d generated -p slash.navigation.tcx.binding2 c:\Development\RouteConverter\navigation-formats\src\main\doc\tcx\TrainingCenterDatabasev2.xsd
+bin\xjc -verbose -d generated -p slash.navigation.kml.binding20 c:\Development\RouteConverter\kml\src\main\doc\kml20.xsd
+bin\xjc -verbose -d generated -p slash.navigation.kml.binding21 c:\Development\RouteConverter\kml\src\main\doc\kml21.xsd
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\kml\src\main\java c:\Development\RouteConverter\kml\src\main\doc\ogckml22.xsd -b c:\Development\RouteConverter\kml\src\main\doc\ogckml22.xjb
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\kml\src\main\java c:\Development\RouteConverter\kml\src\main\doc\kml22gx.xsd -b c:\Development\RouteConverter\kml\src\main\doc\kml22gx.xjb
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\kml\src\main\java c:\Development\RouteConverter\kml\src\main\doc\kml22beta.xsd -b c:\Development\RouteConverter\kml\src\main\doc\kml22beta.xjb
+bin\xjc -verbose -d generated -p slash.navigation.gopal.binding3 c:\Development\RouteConverter\navigation-formats\src\main\doc\gopal\gopal3.xsd
+bin\xjc -verbose -d generated -p slash.navigation.gopal.binding5 c:\Development\RouteConverter\navigation-formats\src\main\doc\gopal\gopal5.xsd
+bin\xjc -verbose -dtd -d generated -p slash.navigation.viamichelin.binding c:\Development\RouteConverter\navigation-formats\src\main\doc\viamichelin\export.dtd
+bin\xjc -verbose -d generated -p slash.navigation.geonames.binding c:\Development\RouteConverter-git\geonames\src\main\doc\geonames.xsd
+bin\xjc -verbose -d generated -p slash.navigation.nmn.binding7 c:\Development\RouteConverter\navigation-formats\src\main\doc\nmn\nmn7.xsd
+bin\xjc -verbose -d generated -p slash.navigation.klicktel.binding c:\Development\RouteConverter\navigation-formats\src\main\doc\klicktel\klicktel.xsd
+bin\xjc -verbose -d generated -p slash.navigation.googlemaps.elevation c:\Development\RouteConverter\googlemaps\src\main\doc\elevation.xsd
+bin\xjc -verbose -d generated -p slash.navigation.googlemaps.geocode c:\Development\RouteConverter\googlemaps\src\main\doc\geocode.xsd
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\datasource\src\main\java -p slash.navigation.datasources.binding c:\Development\RouteConverter\datasource\src\main\doc\datasource-catalog.xsd
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\download\src\main\java -p slash.navigation.download.queue.binding  c:\Development\RouteConverter\download\src\main\doc\queue.xsd
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\route\src\main\java -p slash.navigation.routes.remote.binding c:\Development\RouteConverter\route\src\main\doc\route-catalog.xsd
+bin\xjc -cp lib\jaxb-impl.jar -verbose -d c:\Development\RouteConverter\browser-mapview\src\main\java -p slash.navigation.mapview.tileserver.binding c:\Development\RouteConverter\browser-mapview\src\main\doc\tileserver-catalog.xsd
 
 
