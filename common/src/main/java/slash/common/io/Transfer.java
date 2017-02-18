dba757d50d3971c6a64ea3a6aa26110113dbfb04
Christian Pesch <Christian_Pesch@gmx.de>
2017-02-18 22:44:07 +0100

    make tests green

--- common/src/main/java/slash/common/io/Transfer.java
+++ common/src/main/java/slash/common/io/Transfer.java
@@ -105,6 +105,7 @@ public class Transfer {
     public static String trim(String string, int length) {
         if (string == null)
             return null;
+        string = trim(string);
         return string.substring(0, min(string.length(), length));
     }
 
