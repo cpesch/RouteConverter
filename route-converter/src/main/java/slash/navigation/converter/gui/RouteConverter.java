58e106e1b8e5065d8e07da771a61a3cf802a8334
Christian Pesch <Christian_Pesch@gmx.de>
2018-06-14 14:23:33 +0200

    exclude Java 8 only on Windows

--- route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter.java
+++ route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter.java
@@ -252,7 +252,7 @@ public class RouteConverter extends SingleFrameApplication {
             System.exit(7);
         }
 
-        if (isWindows() && currentVersion.equals("1.8.0_161") || currentVersion.equals("1.8.0_162") || currentVersion.equals("1.8.0_171") || currentVersion.equals("1.8.0_172")) {
+        if (isWindows() && (currentVersion.equals("1.8.0_161") || currentVersion.equals("1.8.0_162") || currentVersion.equals("1.8.0_171") || currentVersion.equals("1.8.0_172"))) {
             showMessageDialog(null, "Java " + currentVersion + " contains a fatal bug in JavaFX on Windows. Please update to Java 8 Update 152 or Java 9 or 10.", "RouteConverter", ERROR_MESSAGE);
             System.exit(8);
         }
