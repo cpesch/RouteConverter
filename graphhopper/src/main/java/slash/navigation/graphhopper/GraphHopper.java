fa47f9d1ea80f4ce2af323b0dcd4061858b65f36
Christian Pesch <Christian_Pesch@gmx.de>
2018-08-21 16:16:25 +0200

    extract BYTES_FOR_EDGE_FLAGS constant

--- graphhopper/src/main/java/slash/navigation/graphhopper/GraphHopper.java
+++ graphhopper/src/main/java/slash/navigation/graphhopper/GraphHopper.java
@@ -74,6 +74,7 @@ public class GraphHopper implements RoutingService {
     private static final TravelMode CAR = new TravelMode("Car");
     private static final List<TravelMode> TRAVEL_MODES = asList(new TravelMode("Bike"), CAR, new TravelMode("Foot"));
     // omitted: Bike2, Hike, MotorCycle, MTB, RacingBike
+    private static final int BYTES_FOR_EDGE_FLAGS = 4;
 
     private final DownloadManager downloadManager;
     private DataSource dataSource;
@@ -225,7 +226,7 @@ public class GraphHopper implements RoutingService {
                 hopper = new GraphHopperOSM().
                         setOSMFile(file.getAbsolutePath()).
                         forDesktop().
-                        setEncodingManager(new EncodingManager(getAvailableTravelModeNames())).
+                        setEncodingManager(new EncodingManager(getAvailableTravelModeNames(), BYTES_FOR_EDGE_FLAGS)).
                         setCHEnabled(false).
                         setEnableInstructions(false).
                         setGraphHopperLocation(path.getAbsolutePath()).
