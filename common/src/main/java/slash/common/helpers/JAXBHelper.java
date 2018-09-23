5b5d6db1fad6a4890489dd0e854cefe7f45b698e
Christian Pesch <Christian_Pesch@gmx.de>
2018-09-23 17:57:49 +0200

    make test green

--- common/src/main/java/slash/common/helpers/JAXBHelper.java
+++ common/src/main/java/slash/common/helpers/JAXBHelper.java
@@ -23,6 +23,7 @@ package slash.common.helpers;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 import javax.xml.bind.Unmarshaller;
 import java.util.HashMap;
 import java.util.List;
@@ -70,14 +71,20 @@ public class JAXBHelper {
     }
 
     public static Marshaller newMarshaller(JAXBContext context) {
+        Marshaller result;
         try {
-            Marshaller result = context.createMarshaller();
+            result = context.createMarshaller();
             result.setProperty(JAXB_FORMATTED_OUTPUT, preferences.getBoolean("prettyPrintXml", true));
-            result.setProperty(JAXB_IMPL_HEADER, HEADER_LINE);
-            return result;
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
+
+        try {
+            result.setProperty(JAXB_IMPL_HEADER, HEADER_LINE);
+        } catch (PropertyException e) {
+            // intentionally left empty
+        }
+        return result;
     }
 
     public static Unmarshaller newUnmarshaller(JAXBContext context) {
