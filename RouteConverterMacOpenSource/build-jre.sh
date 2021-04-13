184e99d48f02699ce41149d3eb0ab018c1c69f00
Christian Pesch <Christian_Pesch@gmx.de>
2021-04-13 08:34:44 +0200

    package JRE 11.0.10 for macOS

---
+++ RouteConverterMacOpenSource/build-jre.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+JDK=jdk-11.0.10.jdk
+JRE=Runtime/Contents/Home
+
+echo Cleaning
+rm -rf $JRE
+
+echo Building
+/Library/Java/JavaVirtualMachines/$JDK/Contents/Home/bin/jlink --module-path "%JDK%/jmods" --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.logging,java.management,java.naming,java.prefs,java.security.jgss,java.sql,java.xml,java.xml.crypto,jdk.unsupported --output $JRE --strip-debug --compress 2 --no-header-files --no-man-pages
+
+echo Reducing
+rm -rf $JRE/legal
+rm -f $JRE/release
+rm -f $JRE/bin/keytool
+rm -f $JRE/conf/security/policy/README.txt
+rm -f $JRE/lib/server/Xusage.txt
+
+echo Zipping
+zip -9 -r jlink-routeconverter-opensource-$JDK-osx.zip Runtime
+
+echo Ready
