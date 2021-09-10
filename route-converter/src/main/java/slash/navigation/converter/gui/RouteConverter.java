416f0c7c697edf3235100cb69e3412cead97ac91
Christian Pesch <Christian_Pesch@gmx.de>
2021-09-10 10:23:56 +0200

    block usage of Java 15 and later due to breaking changes affecting the MacOS integration and the l8n bundles
    (cherry picked from commit a6fea40b75e5c30d992582fd421eb370db37dc1f)

--- route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter.java
+++ route-converter/src/main/java/slash/navigation/converter/gui/RouteConverter.java
@@ -227,13 +227,18 @@ public abstract class RouteConverter extends SingleFrameApplication {
         }
 
         if (!isCurrentAtLeastMinimumVersion(currentVersion, "1.8.0")) {
-            showMessageDialog(null, "Java " + currentVersion + " is too old for FIT and EclipseLink. Please install Java 8 or 10.", "RouteConverter", ERROR_MESSAGE);
+            showMessageDialog(null, "Java " + currentVersion + " is too old for FIT and EclipseLink. Please install Java 8 or 11.", "RouteConverter", ERROR_MESSAGE);
             System.exit(8);
         }
 
         if (isWindows() && (currentVersion.equals("1.8.0_161") || currentVersion.equals("1.8.0_162") || currentVersion.equals("1.8.0_171") || currentVersion.equals("1.8.0_172"))) {
-            showMessageDialog(null, "Java " + currentVersion + " contains a fatal bug in JavaFX on Windows. Please install Java 8 Update 181 or Java 10.", "RouteConverter", ERROR_MESSAGE);
+            showMessageDialog(null, "Java " + currentVersion + " contains a fatal bug in JavaFX on Windows. Please install Java 8 Update 181 or Java 11.", "RouteConverter", ERROR_MESSAGE);
             System.exit(9);
+        }
+
+        if (isJava15OrLater()) {
+            showMessageDialog(null, "Java " + currentVersion + " contains breaking changes. Please install Java 8 or 11.", "RouteConverter", ERROR_MESSAGE);
+            System.exit(10);
         }
     }
 
