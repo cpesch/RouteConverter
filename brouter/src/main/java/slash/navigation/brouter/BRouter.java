1bb2bec7ebb9e9b18fd68586ce028496b824a22d
Christian Pesch <Christian_Pesch@gmx.de>
2022-01-04 20:17:12 +0100

    remove 4

--- brouter/src/main/java/slash/navigation/brouter/BRouter.java
+++ brouter/src/main/java/slash/navigation/brouter/BRouter.java
@@ -161,7 +161,7 @@ public class BRouter extends BaseRoutingService {
     }
 
     private java.io.File getSegmentsDirectory() {
-        return getDirectory(getSegments(), "segments4");
+        return getDirectory(getSegments(), "segments");
     }
 
     private java.io.File createSegmentFile(String key) {
