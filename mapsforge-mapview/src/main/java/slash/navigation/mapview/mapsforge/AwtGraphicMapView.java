cd34ccbdc43cd0071321ba10efdff2a1de710d94
Christian Pesch <christian_pesch@gmx.de>
2024-08-26 18:35:47 +0200

    remove commons stripped dependencies
    update mapsforge to 0.21.0

--- mapsforge-mapview/src/main/java/slash/navigation/mapview/mapsforge/AwtGraphicMapView.java
+++ mapsforge-mapview/src/main/java/slash/navigation/mapview/mapsforge/AwtGraphicMapView.java
@@ -24,7 +24,6 @@ import org.mapsforge.core.graphics.GraphicFactory;
 import org.mapsforge.core.model.BoundingBox;
 import org.mapsforge.core.model.Dimension;
 import org.mapsforge.core.model.LatLong;
-import org.mapsforge.core.util.Parameters;
 import org.mapsforge.map.awt.graphics.AwtGraphicFactory;
 import org.mapsforge.map.controller.FrameBufferController;
 import org.mapsforge.map.controller.LayerManagerController;
@@ -39,7 +38,9 @@ import org.mapsforge.map.scalebar.DefaultMapScaleBar;
 import org.mapsforge.map.scalebar.MapScaleBar;
 import org.mapsforge.map.util.MapPositionUtil;
 import org.mapsforge.map.util.MapViewProjection;
-import org.mapsforge.map.view.*;
+import org.mapsforge.map.view.FpsCounter;
+import org.mapsforge.map.view.FrameBuffer;
+import org.mapsforge.map.view.FrameBufferHA3;
 
 import java.awt.*;
 
@@ -68,11 +69,7 @@ public class AwtGraphicMapView extends Container implements org.mapsforge.map.vi
         this.model = new Model();
 
         this.fpsCounter = new FpsCounter(GRAPHIC_FACTORY, model.displayModel);
-        if (Parameters.FRAME_BUFFER_HA3) {
-            this.frameBuffer = new FrameBufferHA3(this.model.frameBufferModel, this.model.displayModel, GRAPHIC_FACTORY);
-        } else {
-            this.frameBuffer = new FrameBufferOld(this.model.frameBufferModel, this.model.displayModel, GRAPHIC_FACTORY);
-        }
+        this.frameBuffer = new FrameBufferHA3(this.model.frameBufferModel, this.model.displayModel, GRAPHIC_FACTORY);
         this.frameBufferController = FrameBufferController.create(frameBuffer, model);
 
         this.layerManager = new LayerManager(this, model.mapViewPosition, GRAPHIC_FACTORY);
